ce3b3178460f15378c9abecf18d5e1497736b644
Kalle Nilver <kalle@nilver.se>
2017-03-20 20:01:01 +0100

    step 1 and 2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+
   def index
+  end
+
+  def reduce
+  end
+
+  def offset
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+
+  get '', to: 'welcome#index'
+  get 'step_1_reduce', to: 'welcome#reduce'
+  get 'step_2_offset', to: 'welcome#offset'
+  get 'step_3_impact', to: 'welcome#impact'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
