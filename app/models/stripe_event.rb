09f754c08b78f471bda0c5bc13ccc105c594f1ed
Kalle Nilver <kalle@nilver.se>
2017-04-09 09:41:15 +0200

    prevent duplicates of stripe event id

--- app/models/stripe_event.rb
+++ app/models/stripe_event.rb
@@ -9,14 +9,14 @@ class StripeEvent < ApplicationRecord
     list["data"].each do |e| 
       puts e.data.object.object
       event_object = e.data.object
-      if event_object.object == "charge" 
+      if event_object.object == "charge" && StripeEvent.where(stripe_event_id: event_object.id).empty?
         StripeEvent.create( 
-          stripe_event_id: event_object.id, 
+          stripe_event_id: event_object.id,
           stripe_customer_id: event_object.customer, 
           stripe_object: event_object.object,
           stripe_amount: event_object.amount,
           stripe_created: event_object.created
-          )
+        )
       end
     end
   end
