f33267deb2c0deb5e96787c00033cf9b12ec1bf0
Kalle Nilver <kalle@nilver.se>
2017-04-11 21:09:41 +0200

    no message

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,15 @@ class DashboardController < ApplicationController
   def index
   	@total = StripeEvent.where(stripe_object: "charge").sum("stripe_amount").to_i / 100
 
-    @total_contributed = StripeEvent.where(stripe_object: "charge").where(stripe_customer_id: current_user.stripe_customer_id).sum("stripe_amount").to_i / 100
+    @my_amount_invested = StripeEvent.where(stripe_object: "charge").where(stripe_customer_id: current_user.stripe_customer_id).sum("stripe_amount").to_i / 100
+
+    @my_neutral_months = StripeEvent.where(stripe_object: "charge").where(stripe_customer_id: current_user.stripe_customer_id).count
+
+    @unique_climate_neutral_users = User.distinct.pluck(:stripe_customer_id).count
+
+    @total_climate_neutral_months = StripeEvent.where(stripe_object: "charge").count
+
+
+
   end
 end
