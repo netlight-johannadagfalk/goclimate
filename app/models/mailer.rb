b514b25fb662b4b067c3d673f9db848e9c215527
Kalle Nilver <kalle@nilver.se>
2017-05-20 23:03:11 +0200

    don’t send first success email

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -9,35 +9,38 @@ class Mailer
     climate_neutral_months = StripeEvent.charges(user).count
     total_carbon_offset = Project.all.sum("carbon_offset")
 
-    mail = Mail.new
-    mail.from = Email.new(email: 'info@goclimateneutral.org', name: 'GoClimateNeutral.org')
-    
-    personalization = Personalization.new
-    personalization.to = Email.new(email: user.email)
-    personalization.bcc = Email.new(email: 'info@goclimateneutral.org')
-    personalization.bcc = Email.new(email: 'kalle@nilver.se')
-    personalization.subject = "Stort Tack!"
-    personalization.headers = Header.new(key: 'X-Test', value: 'True')
-    personalization.headers = Header.new(key: 'X-Mock', value: 'False')
+    if climate_neutral_months > 1
 
-    mail.asm = ASM.new(group_id: 16739)
+        mail = Mail.new
+        mail.from = Email.new(email: 'info@goclimateneutral.org', name: 'GoClimateNeutral.org')
+        
+        personalization = Personalization.new
+        personalization.to = Email.new(email: user.email)
+        personalization.bcc = Email.new(email: 'info@goclimateneutral.org')
+        personalization.bcc = Email.new(email: 'kalle@nilver.se')
+        personalization.subject = "Stort Tack!"
+        personalization.headers = Header.new(key: 'X-Test', value: 'True')
+        personalization.headers = Header.new(key: 'X-Mock', value: 'False')
 
-    personalization.substitutions = Substitution.new(key: '%months%', value: climate_neutral_months.to_s)
-    personalization.substitutions = Substitution.new(key: '%tonnes%', value: total_carbon_offset.to_s)
+        mail.asm = ASM.new(group_id: 16739)
 
-    mail.personalizations = personalization
+        personalization.substitutions = Substitution.new(key: '%months%', value: climate_neutral_months.to_s)
+        personalization.substitutions = Substitution.new(key: '%tonnes%', value: total_carbon_offset.to_s)
 
-    mail.template_id = '3401db51-a0df-4dd2-8f9c-d3dfd3c64430'
+        mail.personalizations = personalization
 
-    mail.reply_to = Email.new(email: 'info@goclimateneutral.org')
+        mail.template_id = '3401db51-a0df-4dd2-8f9c-d3dfd3c64430'
 
-    # puts JSON.pretty_generate(mail.to_json)
-    puts mail.to_json
+        mail.reply_to = Email.new(email: 'info@goclimateneutral.org')
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
+        # puts JSON.pretty_generate(mail.to_json)
+        puts mail.to_json
+
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+        puts response.status_code
+        puts response.body
+        puts response.headers
+    end
   end
 end
