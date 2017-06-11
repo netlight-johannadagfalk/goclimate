2f8816317f16960dc5e4c20e9736c4301776889e
Kalle Nilver <kalle@nilver.se>
2017-06-11 23:25:33 +0200

    added production action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ClimateNeutralLife_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'https://www.goclimateneutral.org' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
