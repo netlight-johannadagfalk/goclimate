692210a3ba002eed4607456a1067cf50fa3acf60
Kalle Nilver <kalle@nilver.se>
2017-06-21 20:03:59 +0200

    two button share fix

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
 	$('#shareModal').modal();
 
-	$('#share-facebook').click(function() {
+
+	function shareFacebook() {
 		FB.ui({
 		  method: 'share',
 		  href: 'https://www.goclimateneutral.org',
 		  quote: window.facebookQuote,
 		  hashtag: '#goclimateneutral'
 		}, function(response){});
+	}
+	$('#share-facebook-popup').click(function() {
+		shareFacebook();
+	});
+
+	$('#share-facebook-bottom').click(function() {
+		shareFacebook();
 	});
 });
