24f0253aa547eab1198c57b4caaf73956c8cb323
Kalle Nilver <kalle@nilver.se>
2017-07-29 09:32:55 +0200

    ssl cert veryfying

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -16,4 +16,17 @@ class WelcomeController < ApplicationController
 
   def faq
   end
+
+  def cert1
+    render :inline => 'j3xDCIxTqC4ePhzHBh0hlnY1K9xUSlX2T0DRMEbwR3E.DzPWrVmJmCpmfD6Kfftpgkjs1VOUAFmc-19prvfEi1o' and return
+  end
+
+  def cert2
+    render :inline => '6vlQNuynTXChf3kEYpQEG3BU5wI01x2zXyYOjbM0QqM.DzPWrVmJmCpmfD6Kfftpgkjs1VOUAFmc-19prvfEi1o' and return
+  end
+
+  def cert3
+    render :inline => 'KTen_LB2qGKnnNoi0svsU-EwzBDk3CipUaIurr2RF9o.DzPWrVmJmCpmfD6Kfftpgkjs1VOUAFmc-19prvfEi1o' and return
+  end
+
 end
