f0adca21f75f11a58aa913b3b5329a2a25be0eaa
Kalle Nilver <kalle@nilver.se>
2017-08-18 22:14:27 +0200

    dashboard user toplist show and hide

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -17,4 +17,16 @@ $(document).ready(function() {
 	$('#share-facebook-bottom').click(function() {
 		shareFacebook();
 	});
+
+	$('#user_toplist_collapsed').on('hidden.bs.collapse', function () {
+	  $('#user_toplist_open_link').show();
+	  $('#user_toplist_collapse_link').hide();
+	});
+
+	$('#user_toplist_collapsed').on('show.bs.collapse', function () {
+	  $('#user_toplist_open_link').hide();
+	  $('#user_toplist_collapse_link').show();
+	});
+
+	$('#user_toplist_collapse_link').hide();
 });
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -27,11 +27,11 @@ class DashboardController < ApplicationController
 
     @total_climate_neutral_months = StripeEvent.where(stripe_object: "invoice").count
 
-    @user_top_list = User.where("users.stripe_customer_id != ''").where("users.user_name != ''").left_joins(:stripe_events).select("users.id, COUNT(1)").group("users.id").order('COUNT(1) DESC')
+    @user_top_list = User.where("users.stripe_customer_id != ''").left_joins(:stripe_events).select("users.id, COUNT(1)").group("users.id").order('COUNT(1) DESC')
 
-    @country_top_list = User.where("users.stripe_customer_id != ''").left_joins(:stripe_events).select("users.country, COUNT(1)").group("users.country").order('COUNT(1) DESC').limit(10)
+    @country_top_list = User.where("users.stripe_customer_id != ''").left_joins(:stripe_events).select("users.country, COUNT(1)").group("users.country").order('COUNT(1) DESC')
 
-    @projects = Project.all.order(created_at: :desc)
+    @projects = Project.all.order(created_at: :desc).limit(5);
 
     @total_carbon_offset = Project.all.sum("carbon_offset")
 
