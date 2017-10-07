9d0fce0c874f24f3a79b997154319511e8e7be08
Kalle Nilver <kalle@nilver.se>
2017-10-07 14:33:21 +0200

    removed sek and usd parts of dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,21 +10,18 @@ class DashboardController < ApplicationController
       StripeEvent.try_updating_events current_user
     end
 
-    @total_usd = StripeEvent.total_in_usd
-    @total_sek = StripeEvent.total_in_sek
     @total_carbon_offset = Project.total_carbon_offset
 
     my_amount_invested_usd_part = StripeEvent.payments(current_user).where(paid: true).where(currency: "usd").sum("stripe_amount").to_i / 100
     my_amount_invested_sek_part = StripeEvent.payments(current_user).where(paid: true).where(currency: "sek").sum("stripe_amount").to_i / 100
-    @my_amount_invested_usd = (my_amount_invested_usd_part + my_amount_invested_sek_part / 8.7).round
-    @my_amount_invested_sek = (my_amount_invested_sek_part + my_amount_invested_usd_part * 8.7).round    
+    @my_amount_invested_sek = (my_amount_invested_sek_part + my_amount_invested_usd_part * 8.7).round 
+
+    @my_carbon_offset = (@my_amount_invested_sek / LifestyleChoice::SEK_PER_TONNE.to_f).round(1)
 
     @my_neutral_months = StripeEvent.payments(current_user).where(paid: true).count
     @my_neutral_months = @my_neutral_months == 0 ? 1 : @my_neutral_months
 
     @unique_climate_neutral_users = User.distinct.pluck(:stripe_customer_id).count
-
-    @total_climate_neutral_months = StripeEvent.where(stripe_object: "charge").where(paid: true).count
 
     @user_top_list = User.where("users.stripe_customer_id != ''").left_joins(:stripe_events).where("stripe_events.paid = true").select("users.id, COUNT(1)").group("users.id").order('COUNT(1) DESC')
 
