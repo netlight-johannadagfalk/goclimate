6cf2fc33e4bf766e47430e579c9c2f5b0ce0402a
Stefan Lindbohm <stefan@kolizz.com>
2018-08-17 11:09:37 +0200

    Fix deprectation warnings for DashboardController
    Raw SQL is now unallowed by default for ActiveRecord `order`s. What
    we're doing is safe so this change communicates this with `Arel.sql`.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,9 @@
-class DashboardController < ApplicationController
+require 'uri'
 
+class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
-    require 'uri'
-
     if StripeEvent.payments(current_user).count == 0 && Rails.env.production?
         StripeEvent.try_updating_events current_user
     end
@@ -24,9 +22,19 @@ class DashboardController < ApplicationController
 
     @unique_climate_neutral_users = User.distinct.pluck(:stripe_customer_id).count
 
-    @user_top_list = User.where("users.stripe_customer_id != ''").left_joins(:stripe_events).where("stripe_events.paid = true").select("users.id, users.user_name, COUNT(1)").group("users.id").order('COUNT(1) DESC')
+    @user_top_list = User.where("users.stripe_customer_id != ''")
+      .left_joins(:stripe_events)
+      .where("stripe_events.paid = true")
+      .select("users.id, users.user_name, COUNT(1)")
+      .group("users.id")
+      .order(Arel.sql("COUNT(1) DESC"))
 
-    @country_top_list = User.where("users.stripe_customer_id != ''").left_joins(:stripe_events).where("stripe_events.paid = true").select("users.country, COUNT(1)").group("users.country").order('COUNT(1) DESC')
+    @country_top_list = User.where("users.stripe_customer_id != ''")
+      .left_joins(:stripe_events)
+      .where("stripe_events.paid = true")
+      .select("users.country, COUNT(1)")
+      .group("users.country")
+      .order(Arel.sql("COUNT(1) DESC"))
 
     @projects = Project.all.order(created_at: :desc).limit(5);
 
@@ -38,6 +46,5 @@ class DashboardController < ApplicationController
     @encoded_social_quote = URI.encode(@social_quote + ' -> ' + I18n.t('goclimateneutral_url'))
 
     @should_show_share_popup = current_user.last_seen_at < 24.hour.ago
-
   end
 end
