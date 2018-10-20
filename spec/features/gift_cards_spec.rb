081ea067741562a605197e91a2c1d49fb0c1b8f3
Stefan Lindbohm <stefan@kolizz.com>
2018-10-20 13:29:18 +0200

    Fix formatting

--- spec/features/gift_cards_spec.rb
+++ spec/features/gift_cards_spec.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "Gift cards", type: :feature, js: true do
-  scenario "Purchase gift card" do
+RSpec.feature 'Gift cards', type: :feature, js: true do
+  scenario 'Purchase gift card' do
     # Gift cards landing page
     visit '/gift_cards'
     click_link '3 months'
 
     # Checkout page
-    click_button "Purchase Gift Card"
+    click_button 'Purchase Gift Card'
     within_frame(0) do
       find('input[placeholder=Email]').send_keys('featurespec@example.com')
       find('input[placeholder="Card number"]').send_keys('4242424242424242')
       find('input[placeholder="MM / YY"]').send_keys '522'
       find('input[placeholder=CVC]').send_keys '123'
-      click_button "Pay"
+      click_button 'Pay'
     end
 
     # Wait for success page to render
