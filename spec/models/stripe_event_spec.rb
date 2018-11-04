b5436fb4bcb3a145e2b5ab51a22baa37bf67483d
Stefan Lindbohm <stefan@kolizz.com>
2018-11-04 12:33:56 +0100

    Restructure StripeEvent spec to better describe intent

--- spec/models/stripe_event_spec.rb
+++ spec/models/stripe_event_spec.rb
@@ -3,20 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe StripeEvent do
-  before do
-    @u = create(:user, stripe_customer_id: 'customer_test_id')
-    allow(Stripe::Event).to receive(:list).and_return(stripe_json_fixture('stripe_events_list_paid_charge.json'))
-  end
-
   describe '#update_events' do
+    let(:user) do
+      create(:user, stripe_customer_id: 'customer_test_id')
+    end
+
+    before do
+      allow(Stripe::Event).to receive(:list).and_return(stripe_json_fixture('stripe_events_list_paid_charge.json'))
+    end
+
+    it 'creates a StripeEvent for event in Stripe' do
+      StripeEvent.update_events
+
+      expect(StripeEvent.last.stripe_event_id).to eq('test_charge_id')
+      expect(StripeEvent.last.gift_card).to eq(false)
+    end
+
+    it 'does not create duplicate events when called twice' do
+      StripeEvent.update_events
+      StripeEvent.update_events
+
+      expect(StripeEvent.count).to eq(1)
+    end
+
     context 'when Stripe returns list including gift card' do
       before do
         allow(Stripe::Event).to receive(:list).and_return(stripe_json_fixture('stripe_events_list_gift_card.json'))
       end
 
-      it 'inserts one StripeEvent' do
-        @u = create(:user, stripe_customer_id: nil)
-
+      it 'creates a gift card StripeEvent' do
         StripeEvent.update_events
 
         expect(StripeEvent.count).to eq(1)
@@ -26,46 +41,46 @@ RSpec.describe StripeEvent do
       end
     end
 
-    it 'inserts one stripeevent' do
-      StripeEvent.update_events
-
-      expect(Stripe::Event).to have_received(:list).with(limit: 1000)
-      expect(StripeEvent.last.stripe_event_id).to eq('test_charge_id')
-      expect(StripeEvent.last.gift_card).to eq(false)
-    end
-
     context 'when Stripe returns list including unpaid charge' do
       before do
         allow(Stripe::Event).to receive(:list).and_return(stripe_json_fixture('stripe_events_list_unpaid_charge.json'))
       end
 
-      it 'should add a stripeevent' do
+      it 'creates a StripeEvent' do
         StripeEvent.update_events
 
         expect(StripeEvent.last.stripe_event_id).to eq('test_charge_id_2')
       end
     end
 
-    it 'inserts only one stripeevent even if update events is called two times' do
-      StripeEvent.update_events
-      StripeEvent.update_events
-      expect(StripeEvent.count).to eq(1)
-      expect(StripeEvent.last.gift_card).to eq(false)
+    context 'when 1 existing paid charge already exists' do
+      before do
+        create(:stripe_event, stripe_customer_id: 'customer_test_id', paid: true, stripe_object: 'charge', currency: 'usd')
+      end
+
+      it 'sends one more month email' do
+        expect(SubscriptionMailer).to receive_message_chain(:with, :one_more_month_email, :deliver_now)
+          .and_return(SubscriptionMailer.with(email: user.email))
+
+        StripeEvent.update_events
+
+        expect(StripeEvent.count).to eq(2)
+        expect(StripeEvent.last.gift_card).to eq(false)
+      end
     end
 
-    it 'sends one more month email if payment is new' do
-      create(:stripe_event, stripe_customer_id: 'customer_test_id', paid: true, stripe_object: 'charge', currency: 'usd')
-      expect(SubscriptionMailer).to receive_message_chain(:with, :one_more_month_email, :deliver_now).and_return(SubscriptionMailer.with(email: @u.email))
-      StripeEvent.update_events
-      expect(StripeEvent.count).to eq(2)
-      expect(StripeEvent.last.gift_card).to eq(false)
-    end
+    context 'when 11 existing paid charges already exist' do
+      before do
+        create_list(:stripe_event, 11, stripe_customer_id: 'customer_test_id', paid: true, stripe_object: 'charge', currency: 'usd')
+      end
 
-    it 'sends one more year email if one year of payments has gone by' do
-      create_list(:stripe_event, 11, stripe_customer_id: 'customer_test_id', paid: true, stripe_object: 'charge', currency: 'usd')
-      expect(SubscriptionMailer).to receive_message_chain(:with, :one_more_year_email, :deliver_now).and_return(SubscriptionMailer.with(email: @u.email))
-      StripeEvent.update_events
-      expect(StripeEvent.count).to eq(12)
+      it 'sends one more year email' do
+        expect(SubscriptionMailer).to receive_message_chain(:with, :one_more_year_email, :deliver_now).and_return(SubscriptionMailer.with(email: user.email))
+
+        StripeEvent.update_events
+
+        expect(StripeEvent.count).to eq(12)
+      end
     end
 
     context 'when Stripe returns list including event with no Stripe customer' do
