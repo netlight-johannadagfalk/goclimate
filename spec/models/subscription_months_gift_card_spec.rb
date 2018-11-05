24e50c5431b2e1ee9161a60a69c7d5a51f95ed60
Kalle Nilver <kalle@nilver.se>
2018-11-05 23:28:32 +0100

    öre vs kr fix

--- spec/models/subscription_months_gift_card_spec.rb
+++ spec/models/subscription_months_gift_card_spec.rb
@@ -16,15 +16,15 @@ RSpec.describe SubscriptionMonthsGiftCard do
     it 'calculates price correctly for 3 months' do
       gift_card = SubscriptionMonthsGiftCard.new(3, 'sek')
 
-      # 11 tons/year * 40 kr/ton / 12 months * 2 * 100 ore * 3 months
-      expect(gift_card.price).to eq(22_000)
+      # 11 tons/year * 40 kr/ton / 12 months * 2 * 3 months
+      expect(gift_card.price).to eq(220)
     end
 
     it 'calculates price correctly for 6 months' do
       gift_card = SubscriptionMonthsGiftCard.new(6, 'sek')
 
-      # 11 tons/year * 40 kr/ton / 12 months * 2 * 100 ore * 6 months
-      expect(gift_card.price).to eq(44_000)
+      # 11 tons/year * 40 kr/ton / 12 months * 2 * 6 months
+      expect(gift_card.price).to eq(440)
     end
 
     it 'rounds to something'
