651c832303aa087c5b928bd7b96839e58275b922
Kalle Nilver <kalle@nilver.se>
2018-11-05 22:52:12 +0100

    send language specific gift cards

--- spec/mailers/gift_card_mailer_spec.rb
+++ spec/mailers/gift_card_mailer_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe GiftCardMailer, type: :mailer do
       expect { GiftCardMailer.with(email: 'test@example.com', number_of_months: '3', filename: 'filename.pdf').gift_card_email.deliver_now }.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
 
-    let(:mail) { GiftCardMailer.with(email: 'test@example.com', number_of_months: '3', filename: 'filename.pdf').gift_card_email }
+    let(:mail) { GiftCardMailer.with(email: 'test@example.com', number_of_months: '3', filename: 'testfilename.pdf').gift_card_email }
 
     it 'renders the subject' do
       expect(mail.subject).to eql(I18n.t('gift_card_email_subject'))
@@ -24,6 +24,14 @@ RSpec.describe GiftCardMailer, type: :mailer do
 
     it 'matches number of months' do
       expect(mail.body.encoded).to match('3 climate neutral months.')
+    end
+
+    it 'has the correct attatchment' do
+      expect(mail.attachments.size).to eql(1)
+      attachment = mail.attachments[0]
+      expect(attachment).to be_a_kind_of(Mail::Part)
+      expect(attachment.content_type).to be_start_with('application/pdf;')
+      expect(attachment.filename).to eql('testfilename.pdf')
     end
   end
 end
