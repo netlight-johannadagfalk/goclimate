b1c50ce92a2182af9bfb40f18dca0e39eb627692
Stefan Lindbohm <stefan@kolizz.com>
2018-12-04 10:12:47 +0100

    Consolidate subscriber count logic and add tests for it
    This is a first step to refactoring the logic to account for inactive
    subscriptions.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@example.com" }
     password { 'password' }
+
+    factory :user_with_active_subscription do
+      stripe_customer_id { 'cus_TEST' }
+    end
   end
 end
