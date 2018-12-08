7ccae64bdbbf452fa7581a910c875dbe227f3e9d
Stefan Lindbohm <stefan@kolizz.com>
2018-12-09 00:22:08 +0100

    Extract gift card PDF action to its own controller

---
+++ app/controllers/gift_card_certificates_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'gift_card_certificate_pdf_generator'
+
+class GiftCardCertificatesController < ApplicationController
+  def show
+    certificate = GiftCardCertificate.find_by_key(certificate_key)
+    pdf = GiftCardCertificatePDFGenerator.from_certificate(certificate).generate_pdf
+
+    send_data pdf, filename: 'GoClimateNeutral Gift Card.pdf', type: :pdf
+  end
+
+  # This is a preview version of the gift card. Includes sample text and a big EXAMPLE stamp.
+  # Optionally, you can include a subscription_months_to_gift query param.
+  def example
+    number_of_months = params[:subscription_months_to_gift].present? ? params[:subscription_months_to_gift].to_i : 6
+
+    pdf = GiftCardCertificatePDFGenerator.new(
+      message: t('views.gift_cards.example.message_html'),
+      number_of_months: number_of_months,
+      example: true
+    ).generate_pdf
+
+    send_data pdf, filename: 'GoClimateNeutral Gift Card.pdf', type: :pdf, disposition: :inline
+  end
+end
