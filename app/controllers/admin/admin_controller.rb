344a4c890887ba1fc82a1d60d3d53202a5b7efe2
Stefan Lindbohm <stefan@kolizz.com>
2018-12-28 16:44:08 +0100

    Hide admin paths by returning not found if not authorized
    Redirecting gives away information that something is supposed to be on
    the path. While not a big improvement to security it is better to avoid
    giving away information when not necessary.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,9 +2,8 @@
 
 module Admin
   class AdminController < ApplicationController
-    before_action :authenticate_user!
     before_action do
-      redirect_to new_user_session_path unless current_user && (current_user.id == 2 || current_user == 129)
+      render_not_found unless current_user && (current_user.id == 2 || current_user == 129)
     end
   end
 end
