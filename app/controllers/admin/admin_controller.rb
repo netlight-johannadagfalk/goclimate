d68de0b41adb9f6cfa82c0754471194c2336deb4
Kalle Nilver <kalle@nilver.se>
2019-01-27 20:25:00 +0100

    Add admin layout and styling fixes

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,6 +2,7 @@
 
 module Admin
   class AdminController < ApplicationController
+    layout "admin"
     before_action do
       render_not_found unless current_user && (current_user.id == 2 || current_user == 129 || current_user == 4)
     end
