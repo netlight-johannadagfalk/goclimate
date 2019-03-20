cde9f13d2acb7b704595da91e99d42d085c8d251
Stefan Lindbohm <stefan@kolizz.com>
2019-03-20 15:51:59 +0100

    Move domain logic out of receipt view & make it more resilient

--- app/models/subscription_month_receipt.rb
+++ app/models/subscription_month_receipt.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class SubscriptionMonthReceipt
-  attr_reader :date, :id, :amount, :currency
+  attr_reader :stripe_event
 
   def initialize(stripe_event)
-    @id = stripe_event.id
-    @amount = stripe_event.stripe_amount / 100
-    @date = stripe_event.created_at.to_date
-    @currency = stripe_event.currency
+    @stripe_event = stripe_event
   end
 
   def generate_pdf
@@ -16,12 +13,30 @@ class SubscriptionMonthReceipt
         template: 'pdfs/receipt',
         layout: false,
         assigns: {
-          date: @date,
-          currency: @currency,
-          amount: @amount
+          receipt: self
         }
       ),
       orientation: 'portrait'
     )
+  end
+
+  def date
+    stripe_event.created_at.to_date
+  end
+
+  def currency
+    stripe_event.currency
+  end
+
+  def total_amount
+    stripe_event.stripe_amount.to_d / 100
+  end
+
+  def vat_amount
+    (total_amount * (1 - (1 / BigDecimal('1.25')))).round(2)
+  end
+
+  def total_amount_before_vat
+    total_amount - vat_amount
   end
 end
