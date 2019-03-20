ef00284c97dbcfe49e70429acdbc9eac1e1f5b5b
Stefan Lindbohm <stefan@kolizz.com>
2019-03-20 15:50:53 +0100

    Tidy up StripeEvent factory

--- spec/factories/stripe_event.rb
+++ spec/factories/stripe_event.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :stripe_event do |se|
-    se.sequence(:stripe_customer_id) { |n| n }
+  factory :stripe_event do |stripe_event|
+    stripe_event.sequence(:stripe_customer_id) { |n| n }
 
     factory :stripe_event_paid_charge do
       stripe_object { 'charge' }
+      currency { 'sek' }
       paid { true }
 
       factory :stripe_event_monthly do
         stripe_amount { 80_00 }
-        currency { 'sek' }
         gift_card { false }
       end
 
