7d2d75b001545a00af6ba0156c14732f164c0e67
Stefan Lindbohm <stefan@kolizz.com>
2019-03-20 16:03:11 +0100

    Authorize downloads of receipts to user's own charges only

--- app/controllers/users/receipts_controller.rb
+++ app/controllers/users/receipts_controller.rb
@@ -2,6 +2,9 @@
 
 module Users
   class ReceiptsController < ApplicationController
+    before_action :set_receipt, only: [:show]
+    before_action :authorize_receipt, only: [:show]
+
     def index
       @receipts = current_user.stripe_events.map do |stripe_events|
         SubscriptionMonthReceipt.new(stripe_events)
@@ -9,8 +12,17 @@ module Users
     end
 
     def show
-      receipt = SubscriptionMonthReceipt.new(StripeEvent.find(params[:stripe_event_id]))
-      send_data receipt.generate_pdf, filename: 'receipt.pdf', type: :pdf
+      send_data @receipt.generate_pdf, filename: 'receipt.pdf', type: :pdf
+    end
+
+    private
+
+    def set_receipt
+      @receipt = SubscriptionMonthReceipt.new(StripeEvent.find(params[:stripe_event_id]))
+    end
+
+    def authorize_receipt
+      render_not_found unless current_user.stripe_customer_id == @receipt.stripe_event.stripe_customer_id
     end
   end
 end
