0ce76558b875c95ddc05230f14983d6701538c41
Stefan Lindbohm <stefan@kolizz.com>
2019-03-22 16:26:59 +0100

    Rewrite business offset calculator to plain Javascript
    No jQuery and no Gon. We don't have any JS compilers yet, so this
    component is the good old style of JS "class".

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,14 +2,24 @@
 
 class BusinessesController < ApplicationController
   def new
-    gon.locale = I18n.locale
-    gon.BUSINESS_SEK_PER_TONNE = LifestyleChoice::BUSINESS_SEK_PER_TONNE
-    gon.SEK_PER_USD = LifestyleChoice::SEK_PER_USD
-    gon.SEK_PER_EUR = LifestyleChoice::SEK_PER_EUR
+    @cost_per_tonne = cost_per_tonne
   end
 
   def create
     @email = params[:email]
     BusinessInterestMailer.notice_email(@email, params[:number_of_employees], I18n.locale).deliver
+  end
+
+  private
+
+  def cost_per_tonne
+    case I18n.locale
+    when :sv
+      LifestyleChoice::BUSINESS_SEK_PER_TONNE
+    when :en
+      LifestyleChoice::BUSINESS_SEK_PER_TONNE / LifestyleChoice::SEK_PER_USD
+    when :de
+      LifestyleChoice::BUSINESS_SEK_PER_TONNE / LifestyleChoice::SEK_PER_EUR
+    end
   end
 end
