6c725a2173307b1a4a9ad877f938874ef7717585
Stefan Lindbohm <stefan@kolizz.com>
2019-03-26 07:50:09 +0100

    Change flight emissions API endpoint to handle full trips

--- app/controllers/api/v1/flight_footprints_controller.rb
+++ app/controllers/api/v1/flight_footprints_controller.rb
@@ -3,13 +3,22 @@
 module Api
   module V1
     class FlightFootprintsController < ApiController
-      REQUIRED_PARAMETERS = [:flight, :origin, :destination, :duration, :cabin_class, :departure_date].freeze
+      REQUIRED_PARAMS = [:cabin_class].freeze
+      REQUIRED_SEGMENT_PARAMS = [:flight, :origin, :destination, :duration, :departure_date].freeze
       VALID_CABIN_CLASSES = %w[economy premium_economy business first].freeze
+      VALID_CURRENCIES = %w[EUR USD SEK NOK].freeze
 
       before_action :validate_show_params, only: :show
 
       def show
-        render json: { footprint: footprint, details_url: root_url }
+        render json: {
+          footprint: footprint,
+          offset_price: {
+            amount: offset_price_amount,
+            currency: 'SEK'
+          },
+          details_url: root_url
+        }
       end
 
       private
@@ -18,7 +27,29 @@ module Api
       # distance-based estimate is more accurate. We should switch to one as soon
       # as we got time to build what we need for it.
       def footprint
-        (params[:duration].to_f / 60 / 60 * 0.2).round(1)
+        @footprint ||= (total_duration.to_f / 60 / 60 * 0.2).round(1)
+      end
+
+      def offset_price_amount
+        (footprint * LifestyleChoice::SEK_PER_TONNE).to_i * 100
+      end
+
+      def total_duration
+        if params[:segments].present?
+          segments_params.map { |segment_params| segment_params[:duration].to_i }.reduce(0, :+)
+        else
+          params[:duration]
+        end
+      end
+
+      def segments_params
+        @segments_params ||= extract_segments_params
+      end
+
+      def extract_segments_params
+        segments_params = []
+        params[:segments].each { |_, segment_params| segments_params << segment_params }
+        segments_params
       end
 
       def validate_show_params
@@ -26,11 +57,35 @@ module Api
       end
 
       def show_params_valid?
-        REQUIRED_PARAMETERS.each do |parameter|
+        REQUIRED_PARAMS.each do |parameter|
           return false unless params[parameter].present?
         end
 
+        return false unless segments_params_valid?
+
         return false unless VALID_CABIN_CLASSES.include?(params[:cabin_class])
+
+        return false unless params[:currency].nil? || VALID_CURRENCIES.include?(params[:currency])
+
+        true
+      end
+
+      def segments_params_valid?
+        if params[:segments].present?
+          params[:segments].each do |_, segment_params|
+            return false unless segment_params_valid?(segment_params)
+          end
+        else
+          return false unless segment_params_valid?(params)
+        end
+
+        true
+      end
+
+      def segment_params_valid?(params)
+        REQUIRED_SEGMENT_PARAMS.each do |parameter|
+          return false unless params[parameter].present?
+        end
 
         begin
           Date.iso8601(params[:departure_date])
--- spec/requests/api/v1/flight_footprints_controller_spec.rb
+++ spec/requests/api/v1/flight_footprints_controller_spec.rb
@@ -6,16 +6,32 @@ RSpec.describe Api::V1::FlightFootprintsController do
   let(:blocket_api_key) { '***REMOVED***' }
   let(:request_params) do
     {
-      flight: 'VY1266',
-      origin: 'ARN',
-      destination: 'BCN',
-      duration: 12_900,
+      segments: {
+        '0': {
+          flight: 'VY1266',
+          origin: 'ARN',
+          destination: 'BCN',
+          duration: 12_900,
+          departure_date: '2019-02-22'
+        }
+      },
       cabin_class: 'economy',
-      departure_date: '2019-02-22'
+      currency: 'SEK'
     }
   end
+  let(:request_params_with_two_segments) do
+    request_params.deep_merge(
+      segments: { '1': {
+        flight: 'VY1265',
+        origin: 'BCN',
+        destination: 'ARN',
+        duration: 12_900,
+        departure_date: '2019-02-25'
+      } }
+    )
+  end
 
-  describe 'GET /v1/flight_footprint' do
+  shared_examples 'GET /v1/flight_footprint' do
     it 'returns 200 OK for successful requests' do
       get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key), params: request_params
 
@@ -28,17 +44,157 @@ RSpec.describe Api::V1::FlightFootprintsController do
       expect(response.parsed_body['footprint']).to eq(0.7)
     end
 
-    it 'includes estimated footprint in response (another example)' do
-      get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
-                                      params: request_params.merge(duration: 15_840)
+    it 'includes offset price in response' do
+      get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key), params: request_params
 
-      expect(response.parsed_body['footprint']).to eq(0.9)
+      expect(response.parsed_body['offset_price']).to be_present
+    end
+
+    it 'includes offset price amount in response' do
+      get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key), params: request_params
+
+      expect(response.parsed_body['offset_price']['amount']).to eq(2800)
+    end
+
+    it 'includes offset price currency in response' do
+      get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key), params: request_params
+
+      expect(response.parsed_body['offset_price']['currency']).to eq('SEK')
     end
 
     it 'includes details URL in response' do
       get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key), params: request_params
 
       expect(response.parsed_body['details_url']).to eq(root_url)
+    end
+
+    context 'when not providing correct attributes' do
+      it 'returns 400 Bad Request when cabin class is not one of valid values' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.merge(cabin_class: 'invalid')
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns 400 Bad Request when currency is not one of valid values' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.merge(currency: 'invalid')
+
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context 'when providing invalid API keys' do
+      it 'returns 401 Unauthorized when not providing an API key' do
+        get '/api/v1/flight_footprint'
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns 401 Unauthorized when providing an invalid API key' do
+        get '/api/v1/flight_footprint', headers: auth_headers('INVALID_KEY')
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'responds with error type when not providing an API key' do
+        get '/api/v1/flight_footprint'
+
+        expect(response.parsed_body['type']).to eq('authentication_error')
+      end
+    end
+  end
+
+  describe 'GET /v1/flight_footprint' do
+    include_examples 'GET /v1/flight_footprint'
+
+    it 'includes estimated footprint in response (another example)' do
+      get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                      params: request_params.deep_merge(segments: { '0': { duration: 15_840 } })
+
+      expect(response.parsed_body['footprint']).to eq(0.9)
+    end
+
+    it 'sums estimated footprint for each segment' do
+      get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                      params: request_params_with_two_segments
+
+      expect(response.parsed_body['footprint']).to eq(1.4)
+    end
+
+    context 'when not providing correct attributes' do
+      it 'returns 400 Bad Request when flight is missing' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.deep_merge(segments: { '0': { flight: nil } })
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns 400 Bad Request when origin is missing' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.deep_merge(segments: { '0': { origin: nil } })
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns 400 Bad Request when duration is missing' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.deep_merge(segments: { '0': { duration: nil } })
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns 400 Bad Request when destination is missing' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.deep_merge(segments: { '0': { destination: nil } })
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns 400 Bad Request when departure date is missing' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.deep_merge(segments: { '0': { departure_date: nil } })
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns 400 Bad Request when departure date is not an ISO 8601 string' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.deep_merge(
+                                          segments: { '0': { departure_date: '0508-2019' } }
+                                        )
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'responds with error type when parameters are missing' do
+        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                        params: request_params.deep_merge(segments: { '0': { flight: nil } })
+
+        expect(response.parsed_body['type']).to eq('invalid_request_error')
+      end
+    end
+  end
+
+  describe 'GET /v1/flight_footprint (deprecated behavior)' do
+    let(:request_params) do
+      {
+        flight: 'VY1266',
+        origin: 'ARN',
+        destination: 'BCN',
+        duration: 12_900,
+        cabin_class: 'economy',
+        departure_date: '2019-02-22'
+      }
+    end
+
+    include_examples 'GET /v1/flight_footprint'
+
+    it 'includes estimated footprint in response (another example)' do
+      get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
+                                      params: request_params.merge(duration: 15_840)
+
+      expect(response.parsed_body['footprint']).to eq(0.9)
     end
 
     context 'when not providing correct attributes' do
@@ -70,20 +226,6 @@ RSpec.describe Api::V1::FlightFootprintsController do
         expect(response).to have_http_status(:bad_request)
       end
 
-      it 'returns 400 Bad Request when cabin class is missing' do
-        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
-                                        params: request_params.except(:cabin_class)
-
-        expect(response).to have_http_status(:bad_request)
-      end
-
-      it 'returns 400 Bad Request when cabin class is not one of valid values' do
-        get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
-                                        params: request_params.merge(cabin_class: 'invalid')
-
-        expect(response).to have_http_status(:bad_request)
-      end
-
       it 'returns 400 Bad Request when departure date is missing' do
         get '/api/v1/flight_footprint', headers: auth_headers(blocket_api_key),
                                         params: request_params.except(:departure_date)
@@ -103,26 +245,6 @@ RSpec.describe Api::V1::FlightFootprintsController do
                                         params: request_params.except(:flight)
 
         expect(response.parsed_body['type']).to eq('invalid_request_error')
-      end
-    end
-
-    context 'when providing invalid API keys' do
-      it 'returns 401 Unauthorized when not providing an API key' do
-        get '/api/v1/flight_footprint'
-
-        expect(response).to have_http_status(:unauthorized)
-      end
-
-      it 'returns 401 Unauthorized when providing an invalid API key' do
-        get '/api/v1/flight_footprint', headers: auth_headers('INVALID_KEY')
-
-        expect(response).to have_http_status(:unauthorized)
-      end
-
-      it 'responds with error type when not providing an API key' do
-        get '/api/v1/flight_footprint'
-
-        expect(response.parsed_body['type']).to eq('authentication_error')
       end
     end
   end
