04e243906c1a53ba5a70de915c959b151e746e98
Stefan Lindbohm <stefan@kolizz.com>
2019-04-03 12:22:40 +0200

    Update to new ICAO detour constants for flight footprint calculation

--- lib/generic_flight_footprint.rb
+++ lib/generic_flight_footprint.rb
@@ -2,18 +2,20 @@
 
 class GenericFlightFootprint
   # rubocop:disable Layout/AlignHash
-  PLF =          0.77          # Passenger load factor
-  CF  =          0.951         # Cargo factor
-  P   =          0.51          # Pre-production factor
-  M   =          2             # Multiplier for non-emission effects
-  EF  =          3.150         # Emission factor (kg CO2e per kg fuel)
+  PLF          = 0.77          # Passenger load factor
+  CF           = 0.951         # Cargo factor
+  P            = 0.51          # Pre-production factor
+  M            = 2             # Multiplier for non-emission effects
+  EF           = 3.150         # Emission factor (kg CO2e per kg fuel)
+  DC_550       = 50            # Detour constant (< 550 km)
+  DC_550_5500  = 100           # Detour constant (550 km - 5500 km)
+  DC_5500      = 125           # Detour constant (> 5500 km)
 
   LONG_HAUL_CONSTANTS = {
     a:           0.000134576,  # Polynomal fit model
     b:           6.1798,
     c:           3446.20,
     s:           280.39,       # Average seat number
-    dc:          125,          # Detour constant
     cw: {                      # Cabin class weight
       economy:   0.800,
       business:  1.54,
@@ -26,7 +28,6 @@ class GenericFlightFootprint
     b:           2.9866,
     c:           1263.42,
     s:           158.44,       # Average seat number
-    dc:          50,           # Detour constant
     cw: {                      # Cabin class weight
       economy:   0.960,
       business:  1.26,
@@ -56,10 +57,20 @@ class GenericFlightFootprint
   private
 
   def calculate(distance, constants)
-    a, b, c, s, dc = constants.values_at(:a, :b, :c, :s, :dc)
+    a, b, c, s = constants.values_at(:a, :b, :c, :s)
     cw = constants[:cw][@cabin_class]
 
-    x = distance + dc
+    x = distance + detour_constant(distance)
     ((a * (x**2) + b * x + c) / (s * PLF)) * CF * cw * (EF * M + P)
+  end
+
+  def detour_constant(distance)
+    if distance < 550
+      DC_550
+    elsif distance > 5500
+      DC_5500
+    else
+      DC_550_5500
+    end
   end
 end
--- spec/lib/generic_flight_footprint_spec.rb
+++ spec/lib/generic_flight_footprint_spec.rb
@@ -11,27 +11,33 @@ RSpec.describe GenericFlightFootprint do
     end
 
     it 'returns correct footprint for medium haul flight' do
-      # ARN-ZRH, 1501 km -> 302 kg co2e
+      # ARN-ZRH, 1501 km -> 313 kg co2e
       flight_footprint = described_class.new(1501, :economy)
-      expect(flight_footprint.footprint).to eq(305)
+      expect(flight_footprint.footprint).to eq(313)
     end
 
     it 'returns correct footprint for short haul flight' do
-      # ARN-MUC, 1317 km -> 277 kg co2e
+      # ARN-MUC, 1317 km -> 284 kg co2e
       flight_footprint = described_class.new(1317, :economy)
-      expect(flight_footprint.footprint).to eq(276)
+      expect(flight_footprint.footprint).to eq(284)
+    end
+
+    it 'returns correct footprint for short haul flight below lowest detour constant limit' do
+      # ARN-MUC, 394 km -> 132 kg co2e
+      flight_footprint = described_class.new(394, :economy)
+      expect(flight_footprint.footprint).to eq(132)
     end
 
     it 'returns correct footprint for business cabin class' do
-      # ARN-MUC, 1317 km -> 362 kg co2e
+      # ARN-MUC, 1317 km -> 373 kg co2e
       flight_footprint = described_class.new(1317, :business)
-      expect(flight_footprint.footprint).to eq(362)
+      expect(flight_footprint.footprint).to eq(373)
     end
 
     it 'returns correct footprint for first cabin class' do
-      # ARN-MUC, 1317 km -> 362 kg co2e
+      # ARN-MUC, 1317 km -> 710 kg co2e
       flight_footprint = described_class.new(1317, :first)
-      expect(flight_footprint.footprint).to eq(690)
+      expect(flight_footprint.footprint).to eq(710)
     end
   end
 end
