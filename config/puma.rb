3439b406b49325300d9e4b4e6a84f6095744173d
Stefan Lindbohm <stefan@kolizz.com>
2019-04-04 10:42:03 +0200

    Revert "Limit the amount of queued requests in Puma"
    This ends up marking dynos as failed and takes them out of service at
    Heroku.
    
    This reverts commit 0f74e416302fcf36225921462438ddadcb525ff2.

--- config/puma.rb
+++ config/puma.rb
@@ -25,10 +25,6 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 #
 workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 
-# Limit the amount of queued requests so as to not end up with a huge backlog
-# that we can never recover from
-backlog 32
-
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
