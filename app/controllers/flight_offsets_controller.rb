44243ff26f364d3d425439ebbef2e1236bd7c4a3
Kalle Nilver <kalle@nilver.se>
2019-04-04 21:27:31 +0200

    Add co2 and cost to flight offset payment form

--- app/controllers/flight_offsets_controller.rb
+++ app/controllers/flight_offsets_controller.rb
@@ -3,7 +3,7 @@
 class FlightOffsetsController < ApplicationController
   def new
     @offset_params = FlightOffsetParameters.from_s(params[:offset_params])
-    @footprint = FlightFootprint.new({cabin_class: @offset_params.cabin_class, segments: @offset_params.segments})
+    @footprint = FlightFootprint.new(cabin_class: @offset_params.cabin_class, segments: @offset_params.segments)
     @price = (@footprint.footprint.to_f / 1000 * LifestyleChoice::SEK_PER_TONNE).to_i * 100
     @projects = Project.order(id: :desc).limit(2)
   end
