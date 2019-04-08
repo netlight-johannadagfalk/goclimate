e6e998802b868e0d720ac25f35cf3a284a3caf9c
Kalle Nilver <kalle@nilver.se>
2019-04-08 15:52:59 +0200

    Possibility for finn to send num_persons to flight offset

--- app/controllers/flight_offsets_controller.rb
+++ app/controllers/flight_offsets_controller.rb
@@ -2,9 +2,13 @@
 
 class FlightOffsetsController < ApplicationController
   def new
+    @num_persons = (params[:num_persons] || 1).to_i
     @offset_params = FlightOffsetParameters.from_s(params[:offset_params])
-    @footprint = FlightFootprint.new(cabin_class: @offset_params.cabin_class, segments: @offset_params.segments)
-    @price = (@footprint.footprint.to_f / 1000 * LifestyleChoice::SEK_PER_TONNE).to_i * 100
+
+    footprint = FlightFootprint.new(cabin_class: @offset_params.cabin_class, segments: @offset_params.segments)
+
+    @total_footprint = (footprint.footprint.to_f / 1000).to_f * @num_persons
+    @price = (@total_footprint * LifestyleChoice::SEK_PER_TONNE).to_i * 100
     @projects = Project.order(id: :desc).limit(2)
   end
 
