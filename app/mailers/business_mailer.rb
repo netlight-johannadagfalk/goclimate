a15bef8bba56936efa8865860580ea5e4f3c4196
Stefan Lindbohm <stefan@kolizz.com>
2019-05-17 16:19:37 +0200

    Stop sending additional notifications to Kalle
    Because we all need Kalle to keep his peace of mind!

--- app/mailers/business_mailer.rb
+++ app/mailers/business_mailer.rb
@@ -6,7 +6,6 @@ class BusinessMailer < ApplicationMailer
   def employee_offset_notice_email(reply_to, number_of_employees, language)
     mail(
       to: 'info@goclimateneutral.org',
-      cc: 'kalle@goclimateneutral.org',
       reply_to: reply_to,
       subject: 'Intresseanmälan från företag som vill klimatkompensera',
       body: "Intresseanmälan från: #{reply_to}, för #{number_of_employees} antal anställda, språk: #{language}"
