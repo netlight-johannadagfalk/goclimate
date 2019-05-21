4a0dc5472ba4b68bc0d9328fe034d0eeef34aa31
Stefan Lindbohm <stefan@kolizz.com>
2019-05-21 16:05:13 +0200

    Add new relevant fields to climate report

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_141411) do
+ActiveRecord::Schema.define(version: 2019_05_20_135348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 2019_05_15_141411) do
     t.integer "purchased_phones"
     t.integer "purchased_monitors"
     t.integer "other_co2e"
+    t.integer "office_area"
+    t.boolean "consent_to_show_publicly"
+    t.boolean "green_electricity"
+    t.boolean "use_electricity_averages"
+    t.boolean "use_heating_averages"
+    t.integer "number_of_cloud_servers"
+    t.boolean "servers_green_electricity"
+    t.boolean "cloud_servers_green_electricity"
+    t.text "other_specification"
     t.index ["key"], name: "index_climate_reports_on_key"
   end
 
