275fd9c687f62ea33a524527f377d5774a7c01dd
Kalle Nilver <kalle@nilver.se>
2019-05-27 17:07:24 +0200

    Re-introducing offset your employees to welcome business page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,6 +19,7 @@ class WelcomeController < ApplicationController
   end
 
   def business
+    @cost_per_tonne = cost_per_tonne
   end
 
   def contact
@@ -55,5 +56,16 @@ class WelcomeController < ApplicationController
   end
 
   def privacy_policy
+  end
+
+  def cost_per_tonne
+    case I18n.locale
+    when :sv
+      LifestyleChoice::BUSINESS_SEK_PER_TONNE
+    when :en
+      LifestyleChoice::BUSINESS_SEK_PER_TONNE / LifestyleChoice::SEK_PER_USD
+    when :de
+      LifestyleChoice::BUSINESS_SEK_PER_TONNE / LifestyleChoice::SEK_PER_EUR
+    end
   end
 end
