c3b7cf777255f804c3f457261b80b87178ced2af
Kalle Nilver <kalle@nilver.se>
2019-05-29 16:01:02 +0200

    Launch new business page!
    Removes the current/previous page and related views.

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
+# TODO: The name of this controller is made slightly confusing after the
+# introduction of the business namespace and might benefit from being moved
+# into the namespace and getting a more specific name such as
+# Business::EmployeeOffsetsController
 class BusinessesController < ApplicationController
-  def new
-    @cost_per_tonne = cost_per_tonne
-  end
-
   def create
     @email = params[:email]
     BusinessMailer.employee_offset_notice_email(@email, params[:number_of_employees], I18n.locale).deliver
