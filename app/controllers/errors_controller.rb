136d152c102e16173bc63dab24bb016515e0495a
MikaelAmborn <mikael.amborn@gmail.com>
2019-09-17 10:49:25 +0200

    Fix InvalidMimeType on error page for odd requests (#167)
    Use head instead of 'render nothing: true' as it is the preferred rails
    way of rendering empty responses with only status.
    
    Fixes Rollbar issue #137

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -6,21 +6,21 @@ class ErrorsController < ApplicationController
   def not_found
     respond_to do |format|
       format.html { render status: 404 }
-      format.all { render nothing: true, status: 404 }
+      format.all { head 404 }
     end
   end
 
   def unprocessable_entity
     respond_to do |format|
       format.html { render status: 422 }
-      format.all { render nothing: true, status: 422 }
+      format.all { head 422 }
     end
   end
 
   def internal_server_error
     respond_to do |format|
       format.html { render status: 500 }
-      format.all { render nothing: true, status: 500 }
+      format.all { head 500 }
     end
   end
 end
