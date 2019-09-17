533cea53b6b6330ed0c819baf9afaf456207135f
Stefan Lindbohm <stefan@kolizz.com>
2019-09-17 15:54:07 +0200

    Move main styling to Webpack

--- app/assets/bundles/main.js
+++ app/assets/bundles/main.js
@@ -23,3 +23,19 @@ import 'bootstrap-social/bootstrap-social.css';
 /* TODO: Remove once welcome.js is moved to Webpack and can import CountUp directly */
 import CountUp from 'countup.js';
 window.CountUp = CountUp;
+
+/* Global styling */
+import '../stylesheets/base.scss';
+import '../stylesheets/type.scss';
+import '../stylesheets/panels.scss';
+import '../stylesheets/forms.scss';
+import '../stylesheets/layout/content_section.scss';
+import '../stylesheets/components/climate_report_table.scss';
+import '../stylesheets/components/impact_calculator_fields.scss';
+import '../stylesheets/components/navbar_goclimateneutral.scss';
+import '../stylesheets/components/gift_cards_intro.scss';
+import '../stylesheets/components/registrations_signup_form.scss';
+import '../stylesheets/components/business_offset_form.scss';
+import '../stylesheets/components/receipts.scss';
+import '../stylesheets/components/flight_offset_details.scss';
+import '../stylesheets/components/projects_teaser.scss';
