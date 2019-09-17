242ab5a90278632a1b41a4e2f802e2e0146355bf
Stefan Lindbohm <stefan@kolizz.com>
2019-09-17 14:07:22 +0200

    Move Social Buttons for Bootstrap to Webpack

--- app/assets/bundles/main.js
+++ app/assets/bundles/main.js
@@ -12,3 +12,6 @@ import 'font-awesome/scss/font-awesome.scss';
 /* Bootstrap */
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
+
+/* Social Buttons for Bootstrap */
+import 'bootstrap-social/bootstrap-social.css';
