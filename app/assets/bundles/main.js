f313447297fe75071dab3f34a71c39eea08f31fb
Stefan Lindbohm <stefan@goclimateneutral.org>
2019-10-25 15:54:02 +0200

    Recommend regions based on IP & move region switcher to footer
    Introduces a notice strip at the top of the screen if the user seems to
    be in another region than they're visiting, based on the IPCountry
    header from Cloudflare. Dismissable which gets remembered through a
    session cookie.
    
    Also moves the region selector to the footer now that we're likely to
    recommend the correct region anyway. Switching should become a corner
    case. As a bonus, this selector is present even when logged in, which
    wasn't the case before.

--- app/assets/bundles/main.js
+++ app/assets/bundles/main.js
@@ -34,6 +34,7 @@ import '../stylesheets/components/business_offset_form.scss';
 import '../stylesheets/components/receipts.scss';
 import '../stylesheets/components/flight_offset_details.scss';
 import '../stylesheets/components/projects_teaser.scss';
+import '../stylesheets/components/region_recommendation.scss';
 
 /* Components */
 import '../javascripts/components/business_offset_calculator';
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Region = Struct.new(:slug, :locale, :currency, :icon) do
+Region = Struct.new(:name, :slug, :locale, :currency, :icon, :country_codes) do
   def self.all
     [Region::Europe, Region::Germany, Region::Sweden, Region::USA].freeze
   end
@@ -9,12 +9,18 @@ Region = Struct.new(:slug, :locale, :currency, :icon) do
     all.find { |region| region.slug == slug }
   end
 
+  def self.recommended_for_ip_country(country_code)
+    return nil unless country_code.present?
+
+    all.find { |region| region.country_codes&.include?(country_code) } || Region::Europe
+  end
+
   def to_param
     slug
   end
 end
 
-Region::Europe = Region.new(nil, :en, Currency::EUR, 'eu').freeze
-Region::Germany = Region.new('de', :de, Currency::EUR, 'de').freeze
-Region::Sweden = Region.new('se', :sv, Currency::SEK, 'se').freeze
-Region::USA = Region.new('us', :en, Currency::USD, 'us').freeze
+Region::Europe = Region.new('Europe', nil, :en, Currency::EUR, 'eu').freeze
+Region::Germany = Region.new('Deutschland', 'de', :de, Currency::EUR, 'de', %w[DE]).freeze
+Region::Sweden = Region.new('Sverige', 'se', :sv, Currency::SEK, 'se', %w[SE]).freeze
+Region::USA = Region.new('United States', 'us', :en, Currency::USD, 'us', %w[US]).freeze
