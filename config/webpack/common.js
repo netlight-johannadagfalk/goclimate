f332d3ddd0c182dc4b23b8d9c241d304a6a58737
Stefan Lindbohm <stefan@goclimateneutral.org>
2019-10-30 14:49:40 +0100

    Transpile all JS with Babel to enable ES2018 features!

--- config/webpack/common.js
+++ config/webpack/common.js
@@ -52,6 +52,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
         test: /\.css$/,
         use: cssLoaders
       },
