594659dd56a2fc80fcede0b022cbf68d715d7f32
Kalle Nilver <kalle@nilver.se>
2019-12-16 12:10:23 +0100

    Update financial info for transparency page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -41,13 +41,13 @@ class WelcomeController < ApplicationController
     @unique_climate_neutral_users = User.with_active_subscription.count
     @total_carbon_offset = Project.total_carbon_offset
 
-    # as of 191013 - found in https://docs.google.com/spreadsheets/d/1RzUCaVqTTxJYjfUMeh3KlTxBpiATy2ViPO3m0LHGpG8/edit#gid=0
-    @project_cost_in_sek2019 = (-3_325_156 * 0.8).round # exklude VAT
-    @salaries_cost_in_sek2019 = -281_783
-    @other_operating_expenses2019 = (-885_362 * 0.8).round # exklude VAT
+    # as of 191131 - found in https://docs.google.com/spreadsheets/d/1RzUCaVqTTxJYjfUMeh3KlTxBpiATy2ViPO3m0LHGpG8/edit#gid=0
+    @project_cost_in_sek2019 = (-3_962_041 * 0.8).round # exklude VAT
+    @salaries_cost_in_sek2019 = -477_285
+    @other_operating_expenses2019 = (-1_130_093 * 0.8).round # exklude VAT
 
-    # as of 191013 - found at fortnox.se
-    @invoiced2019 = 964_592 # exklude VAT
+    # as of 191131 - found at fortnox.se
+    @invoiced2019 = 1_646_445 # exklude VAT
 
     @payouts_from_users2019 = (StripePayout.where('extract(year from created_at) = 2019').sum(:amount) / 100)
     @total_revenue = @invoiced2019 + @payouts_from_users2019
