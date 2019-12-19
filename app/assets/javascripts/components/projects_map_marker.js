dc6654c3e816b6e8cd06b78c73cae8a056236b40
Stefan Lindbohm <stefan@goclimateneutral.org>
2019-12-19 18:43:07 +0100

    Add map with all projects!
    Many options for transforming coordinates were considered, but in the
    end most had practical problems in terms of how to install dependencies
    & the size of required dependencies. Even though including the proj4js
    library client side might seem a little excessive for this use, other
    options of doing it server-side were prohibitely complicated. A
    trade-off indeed. At least we're not including a full scale web mapping
    solution on the client :).

---
+++ app/assets/javascripts/components/projects_map_marker.js
@@ -0,0 +1,37 @@
+import proj4 from 'proj4';
+
+/* These markers are overlaid on top of world_epsg_32663.png (projected using
+ * the EPSG:32663 coordinate system). This class reads WGS84 (EPSG:4326)
+ * coordinates from data attributes and transforms them to EPSG:32663, then
+ * percentages of the map image, and finally sets those percentages as absolute
+ * positioning. EPSG:32663 has positive values going east and north, so
+ * percentages are from left and bottom. */
+
+const transformer = proj4('EPSG:4326', 'PROJCS["WGS 84 / World Equidistant Cylindrical",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"],AXIS["Latitude",NORTH],AXIS["Longitude",EAST]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Equirectangular"]]');
+
+/* Bounds for world_epsg_32663.png (it does not cover the full coordinate system bounds) */
+const WORLD_IMAGE_LEFT = -20037508.3428;
+const WORLD_IMAGE_BOTTOM = -6620510.8664;
+const WORLD_IMAGE_WIDTH = 40075016.6856;
+const WORLD_IMAGE_HEIGHT = 15929446.9914;
+
+class ProjectsMapMarker {
+  constructor(markerElement) {
+    this.markerElement = markerElement;
+    this.setPosition();
+  }
+
+  setPosition() {
+    const [x, y] = transformer.forward([
+      parseFloat(this.markerElement.dataset.longitude),
+      parseFloat(this.markerElement.dataset.latitude)
+    ]);
+    const imagePercentalX = ((x - WORLD_IMAGE_LEFT) / WORLD_IMAGE_WIDTH) * 100;
+    const imagePercentalY = ((y - WORLD_IMAGE_BOTTOM) / WORLD_IMAGE_HEIGHT) * 100;
+
+    this.markerElement.style.left = `${imagePercentalX}%`;
+    this.markerElement.style.bottom = `${imagePercentalY}%`;
+  }
+}
+
+window.ProjectsMapMarker = ProjectsMapMarker;
