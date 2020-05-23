fd2ae3ce03132dba176c4e38114190cbb6fb4853
kalle <kalle@nilver.se>
2020-05-23 17:36:41 +0200

    Make project optional for invoices
    It's important that we register all offset-invoices in our system to keep track of CO2e left total and left in every project. So far the check that everything is registered has been done manually now and then. By adding all invoices to Invoice, even ones without a project, we can easily track which invoices are not registered yet. This will also have the side effect that we can keep track of business income in our admin.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Invoice < ApplicationRecord
-  belongs_to :project
+  belongs_to :project, optional: true
   validates :certificate_reciever_email, email: true
 end
