netlight-johannadagfalk/goclimate

commit 87ffb77078760ab570a4efb35105614daab17f4c

Remove old calculator (but keep LifestyleChoices model for now)
Keeping the model so that we can migrate all data from it to the new
LifestyleCalculator/LifestyleFootprint models after new signups using
this one has stopped.

app/controllers/users/registrations_controller.rb
@@ -11,15 +11,13 @@ module Users
     # GET /resource/sign_up
     def new
       @user = User.new(region: current_region.id)
+      @footprint_price = SubscriptionManager.price_for_footprint(@footprint.total, current_region.currency)
 
-      if alternative_signup?
-        @footprint_price = SubscriptionManager.price_for_footprint(@footprint.total, current_region.currency)
-        @projects = Project.order(id: :desc).limit(3)
-        @country_average = LifestyleFootprintAverage.find_by_country(@footprint.country)
-      end
+      @projects = Project.order(id: :desc).limit(3)
+      @country_average = LifestyleFootprintAverage.find_by_country(@footprint.country)
 
       respond_to do |format|
-        format.html { render 'new_alternative' if alternative_signup? }
+        format.html { render }
         format.json { render_price_json }
       end
     end
@@ -31,7 +29,7 @@ module Users
     #
     # Rubocop warnings disabled because the "but no simpler" part of that
     # Einstein quote "As simple as possible, but no simpler".
-    def create # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+    def create # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
       render_user_invalid_json && return unless @user.save
 
       sign_in(resource_name, @user, force: true) # Force because we have updated the password
@@ -93,11 +91,8 @@ module Users
     end
 
     def set_footprint_and_price
-      @lifestyle_choice_ids = lifestyle_choice_ids_from_params if params[:choices].present?
-      footprint_key = params[:lifestyle_footprint]
-      @footprint = LifestyleFootprint.find_by_key!(footprint_key) if footprint_key.present?
-
-      @footprint_tonnes = @footprint&.total || LifestyleChoice.lifestyle_choice_footprint(@lifestyle_choice_ids)
+      @footprint = LifestyleFootprint.find_by_key!(params[:lifestyle_footprint])
+      @footprint_tonnes = @footprint&.total
       @subscription_tonnes = @footprint_tonnes * (params[:people].presence&.to_i || 1)
       @plan_price = SubscriptionManager.price_for_footprint(@subscription_tonnes, current_region.currency)
     end
@@ -147,25 +142,6 @@ module Users
 
     def error_json(message)
       { error: { message: message } }
-    end
-
-    def lifestyle_choice_ids_from_params
-      unless params[:choices]&.match(/\d+,\d+,\d+,\d/)
-        respond_to do |format|
-          format.html { redirect_to '/#choose-plan' }
-          format.json do
-            render status: 400,
-                   json: error_json('Something went wrong. Please start over and try again.')
-          end
-        end
-        return
-      end
-
-      @lifestyle_choice_ids = params[:choices].split(',').map(&:to_i)
-    end
-
-    def alternative_signup?
-      @footprint.present? && experiment_active?(:alternative_signup)
     end
   end
 end

Gemfile
@@ -39,7 +39,6 @@ gem 'redcarpet', '~> 3.5.0'
 
 # Views
 gem 'country_select', '~> 4.0.0'
-gem 'gon', '~> 6.3.2'
 gem 'jbuilder', '~> 2.9'
 
 # Geometry

app/assets/bundles/main.js
@@ -46,7 +46,6 @@ import '../main/index.scss';
 import '../javascripts/components/business_offset_calculator';
 import '../javascripts/components/counting_number';
 import '../javascripts/components/projects_map_marker';
-import '../javascripts/welcome';
 import '../javascripts/dashboard';
 import '../javascripts/users';
 import '../javascripts/users/edit_card';

app/assets/bundles/main_old.js
@@ -53,7 +53,6 @@ import '../stylesheets/panels.scss';
 import '../stylesheets/forms.scss';
 import '../stylesheets/layout/content_section.scss';
 import '../stylesheets/components/climate_report_table.scss';
-import '../stylesheets/components/impact_calculator_fields.scss';
 import '../stylesheets/components/navbar_goclimateneutral.scss';
 import '../stylesheets/components/gift_cards_intro.scss';
 import '../stylesheets/components/registrations_signup_form.scss';
@@ -70,7 +69,6 @@ import '../stylesheets/components/tonnes_label.scss';
 import '../javascripts/components/business_offset_calculator';
 import '../javascripts/components/counting_number';
 import '../javascripts/components/projects_map_marker';
-import '../javascripts/welcome';
 import '../javascripts/dashboard';
 import '../javascripts/users';
 import '../javascripts/users/edit_card';

app/assets/javascripts/welcome.js
@@ -1,63 +1,0 @@
-import $ from 'jquery';
-
-function initializeWelcome() {
-  var signUpUrl = $('#custom-plan').attr('href').split('?')[0];
-
-  function planSum(){
-    var food = parseInt($('#food').val());
-    var car = parseInt($('#car').val());
-    var flying = parseInt($('#flying').val());
-    var base_cost = parseInt($('#base_cost').val());
-    var people = parseInt($('#people').val());
-
-    var tonne_co2 = parseFloat(gon.lifestyle_choice_co2[base_cost]) + parseFloat(gon.lifestyle_choice_co2[food]) + parseFloat(gon.lifestyle_choice_co2[car]) + parseFloat(gon.lifestyle_choice_co2[flying]);
-    tonne_co2 = tonne_co2 * gon.lifestyle_choice_co2[people]
-    var rounded_price_with_buffer = 0;
-
-    if (gon.currency === 'usd') {
-      var price = tonne_co2 * gon.SEK_PER_TONNE / gon.SEK_PER_USD / 12;
-      var rounded_price_with_buffer = Math.round((price * gon.BUFFER_SIZE) * 10) / 10;
-      var price_per_tonne = Math.round((gon.SEK_PER_TONNE / gon.SEK_PER_USD) * 10) / 10;
-    } else if (gon.currency === 'eur') {
-      var price = tonne_co2 * gon.SEK_PER_TONNE / gon.SEK_PER_EUR / 12;
-      var rounded_price_with_buffer = Math.round((price * gon.BUFFER_SIZE) * 10) / 10;
-      var price_per_tonne = Math.round((gon.SEK_PER_TONNE / gon.SEK_PER_EUR) * 10) / 10;
-    } else {
-      var price = tonne_co2 * gon.SEK_PER_TONNE / 12;
-      var rounded_price_with_buffer = Math.ceil(price * gon.BUFFER_SIZE / 5) * 5;
-      var price_per_tonne = gon.SEK_PER_TONNE;
-    }
-
-    var data_content = gon.price_info_popup_content.replace('%{footprint}', Math.round(tonne_co2 * 10) / 10);
-    data_content = data_content.replace('%{footprint_x2}', Math.round(tonne_co2 * 10) / 10 * 2);
-    data_content = data_content.replace('%{price_per_tonne}', gon.currency_prefix + price_per_tonne + gon.currency_suffix);
-    data_content = data_content.replace('%{price_per_month}', gon.currency_prefix + rounded_price_with_buffer + gon.currency_suffix);
-
-    $('.custom-plan-price #sum').text(rounded_price_with_buffer);
-    $('#price-info-helper').attr('data-content', data_content);
-    $('.popover-content').html(data_content);
-    $('#custom-plan').attr('href', signUpUrl + '?choices=' + base_cost + ',' + food + ',' + car + ',' + flying + ',' + people);
-  };
-
-  var impactCalculatorFields = document.querySelectorAll('#food, #car, #flying, #people');
-
-  for (var i = 0; i < impactCalculatorFields.length; i++) {
-    impactCalculatorFields[i].addEventListener('change', function() {
-      planSum();
-    });
-  }
-
-  planSum();
-
-  $('[data-toggle="popover"]').popover();
-
-  //cursor: pointer; fixes iOS bootstrap tooltip bug: https://github.com/twbs/bootstrap/issues/16028;
-  $('#price-info-helper').on('show.bs.popover', function () {
-    $('body').css('cursor', 'pointer');
-  });
-  $('#price-info-helper').on('hide.bs.popover', function () {
-    $('body').css('cursor', 'default');
-  });
-};
-
-window.initializeWelcome = initializeWelcome;

app/assets/stylesheets/components/impact_calculator_fields.scss
@@ -1,16 +1,0 @@
-.impact-calculator-fields {
-  .impact-calculator-fields--lifestyle-choice {
-    margin-bottom: 5px;
-  }
-
-  .impact-calculator-fields--price {
-    margin-top: 20px;
-    line-height: 1em;
-    padding: 0;
-    font-size: 18px;
-  }
-
-  .impact-calculator-fields--price-sum {
-    font-weight: bold;
-  }
-}

app/controllers/application_controller.rb
@@ -102,13 +102,6 @@ class ApplicationController < ActionController::Base
   def set_active_experiments
     experiments = ExperimentsAssignment.new(cookies[:experiments])
 
-    # Moving to testing only old calculator with old sign up, or new calculator
-    # with new sign up. So move everyone who has new_calculator set since
-    # before to the latter group. new_calculator frequency is set to 0, so this
-    # only affects cookies set from before this change.
-    # TODO: Remove after we're done testing alternative_signup
-    experiments.enable([:alternative_signup]) if experiments.active_experiments.include?(:new_calculator)
-
     parse_experiments_from_params(experiments)
 
     cookies[:experiments] = { value: experiments.cookie_string, expires: 30.days.from_now }

app/controllers/welcome_controller.rb
@@ -6,10 +6,6 @@ class WelcomeController < ApplicationController
     @total_carbon_offset = Project.total_carbon_offset
     @lifestyle_choice_co2 = LifestyleChoice.lifestyle_choice_co2
     @latest_project = Project.order(date_bought: :desc).first
-
-    gon.lifestyle_choice_co2 = @lifestyle_choice_co2
-    gon.price_info_popup_content = I18n.t('price_info_popup_content')
-    gon_currency_params
   end
 
   def about
@@ -39,16 +35,6 @@ class WelcomeController < ApplicationController
   end
 
   def privacy_policy
-  end
-
-  def gon_currency_params
-    gon.currency = current_region.currency.iso_code
-    gon.currency_prefix = current_region.currency.prefix
-    gon.currency_suffix = current_region.currency.suffix
-    gon.SEK_PER_TONNE = LifestyleChoice::SEK_PER_TONNE
-    gon.BUFFER_SIZE = LifestyleChoice::BUFFER_SIZE
-    gon.SEK_PER_USD = Currency::SEK_PER_USD
-    gon.SEK_PER_EUR = Currency::SEK_PER_EUR
   end
 
   def travel_calculator

commit 014e9007811d20aacc367604fa19ea8facd00093

Fix crash & unexpected behavior on sign up page
Crash introduced by 87ffb77 and caused by having LifestyleFootprints
with a null key. This change also prevents multiple users from signing
up with the same footprint record.

app/controllers/users/registrations_controller.rb
@@ -2,7 +2,6 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController # rubocop:disable Metrics/ClassLength
-    before_action :redirect_invalid_params, only: [:new]
     before_action :set_footprint_and_price, only: [:new, :create]
     before_action :set_user, only: [:create]
     before_action :set_subscription_manager, only: [:create]
@@ -85,12 +84,14 @@ module Users
 
     private
 
-    def redirect_invalid_params
-      redirect_to root_path unless params[:choices].present? || params[:lifestyle_footprint].present?
-    end
-
     def set_footprint_and_price
-      @footprint = LifestyleFootprint.find_by_key!(params[:lifestyle_footprint])
+      unless params[:lifestyle_footprint].present? &&
+             (@footprint = LifestyleFootprint.find_by_key!(params[:lifestyle_footprint])).present? &&
+             @footprint.user_id.nil?
+        redirect_to root_url
+        return
+      end
+
       @footprint_tonnes = @footprint&.total
       @subscription_tonnes = @footprint_tonnes * (params[:people].presence&.to_i || 1)
       @plan_price = SubscriptionManager.price_for_footprint(@subscription_tonnes, current_region.currency)