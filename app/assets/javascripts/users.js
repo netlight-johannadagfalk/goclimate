18563c5cef389db9cb7f3b3f205eb702ac50a0ef
Emma Bäckström <backstromemma@gmail.com>
2020-09-15 16:07:26 +0200

    Remove old video modal js code

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -13,33 +13,6 @@ function initializeUsers() {
   $('#share-facebook-bottom').click(function() {
     shareFacebook();
   });
-
-  if(window.location.href.indexOf("share") == -1) {
-    $('#videoModal').modal('show');
-  };
-
-  $('#show-video').on('click', function (e) {
-    e.preventDefault();
-    $('#videoModal').modal('show');
-    ga('send', {
-      hitType: 'event',
-      eventAction: 'viewVideo'
-    });
-  });
-
-  $('#videoModal').on('hidden.bs.modal', function (e) {
-    var $frame = $('#videoModal iframe');
-
-    // saves the current iframe source
-    var vidsrc = $frame.attr('src');
-
-    // sets the source to nothing, stopping the video
-    $frame.attr('src', '');
-
-    // sets it back to the correct link so that it reloads immediately on the next window open
-    $frame.attr('src', vidsrc);
-
-  });
 };
 
 window.initializeUsers = initializeUsers;
