c543f9f3bcd1858629afb71324f20dc39af785c6
Emma Bäckström <emmabckstrm@users.noreply.github.com>
2020-09-25 10:16:06 +0200

    Add test for lifestyle_footprint.country is nil (#395)

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -23,4 +23,29 @@ RSpec.feature 'Sessions', type: :feature, js: true do
     find('.dashboard-show', wait: 20)
     expect(page).to have_text 'Hello, climate friend!'
   end
+
+  # We have lifestyle footprints in our db where country is nil
+  scenario 'User where lifestyle_footprint.country is nil signs in' do
+    user = create(:user)
+    footprint = user.current_lifestyle_footprint
+    footprint.country = nil
+    footprint.save(:validate => false)
+
+    if page.has_link?('user-menu')
+      click_link('user-menu')
+      click_link('Log out')
+    end
+    # Homepage
+    visit '/'
+    click_link 'Log In'
+
+    # sign in page
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log In'
+
+    # Wait for dashboard to render
+    find('.dashboard-show', wait: 20)
+    expect(page).to have_text 'Hello, climate friend!'
+  end
 end
