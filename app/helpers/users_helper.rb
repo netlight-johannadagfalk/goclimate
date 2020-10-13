a8fb8fa22f15c1424239bac470cbf925b9da6e2a
Emma Bäckström <backstromemma@gmail.com>
2020-10-13 11:46:05 +0200

    Make sure users helper handles empty string

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@
 
 module UsersHelper
   def social_quote_for(name, neutral_months)
-    if name.nil?
+    if name.blank?
       I18n.t('i_have_lived_climate_neutral_for_join_me', count: neutral_months)
     else
       I18n.t('name_have_lived_climate_neutral_for_join_me', count: neutral_months, name: name)
