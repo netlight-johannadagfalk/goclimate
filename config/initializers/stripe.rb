45e403775c911fb3371ee8db6c0583ca647655a2
Stefan Lindbohm <stefan@lindbohm.se>
2020-11-04 17:24:45 +0100

    Improve Stripe logging

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,4 +7,14 @@ Rails.configuration.stripe = {
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 Stripe.api_version = '2019-10-17'
-Stripe.log_level = Stripe::LEVEL_INFO
+Stripe.logger =
+  if Rails.env.development?
+    # Development logger is by default only pointing to the log file, not
+    # stdout. Make sure we see what's going on in stdout instead of the log
+    # file.
+    Logger.new($stdout, level: :info, formatter: Rails.logger.formatter)
+  else
+    # Default in production is debug level, which would log potential sensitive
+    # information in this case. Make sure to not do that.
+    Rails.logger.dup.tap { |l| l.level = Logger::INFO if l.debug? }
+  end
