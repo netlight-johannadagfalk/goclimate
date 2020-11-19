0249af153488ee1dcd78334a289d9a0e151089ca
Stefan Lindbohm <stefan@lindbohm.se>
2020-11-19 09:02:56 +0100

    Remove old world front end

--- config/webpack/common.js
+++ config/webpack/common.js
@@ -99,8 +99,8 @@ module.exports = {
         modules: {
           test: /[\\/]node_modules[\\/](?!bootstrap)/,
           name: 'modules',
-          // Currently undocument feature:  https://github.com/webpack/webpack/pull/6791
-          chunks: (chunk) => chunk.name.match(/^main|main_old$/)
+          // Currently undocumented feature:  https://github.com/webpack/webpack/pull/6791
+          chunks: (chunk) => chunk.name.match(/^main$/)
         }
       },
       name: false
