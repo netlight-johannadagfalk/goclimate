22ce1c9e7dcab4ed86bf3a5434cafd741efae3f8
Emma Bäckström <backstromemma@gmail.com>
2021-02-02 12:50:01 +0100

    Remove AB test for signup without subscription

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -127,7 +127,7 @@ RSpec.feature 'Registrations', type: :feature, js: true do
   end
 
   scenario 'Register without subscription' do
-    visit '/?disable_experiments=remove_signup_without_subscription'
+    visit '/'
 
     select('Sweden', from: 'country')
     click_button 'Get started'
@@ -237,7 +237,7 @@ RSpec.feature 'Registrations', type: :feature, js: true do
 
   scenario 'Register through campaign page without subscription' do
     # Campaign page
-    visit '/know-your-carbon-footprint?disable_experiments=remove_signup_without_subscription'
+    visit '/know-your-carbon-footprint'
 
     within '#first-registration-form' do
       select('Sweden', from: 'country')
