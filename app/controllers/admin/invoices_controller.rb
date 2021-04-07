f1e8a216a31f0dd29b6ec902cc336a62c64dfe18
kalle <kalle@nilver.se>
2021-04-07 14:39:00 +0200

    You should be allowed to choose every project when editing invoice

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -25,7 +25,7 @@ module Admin
 
     # GET /invoices/1/edit
     def edit
-      @available_projects = Project.order(created_at: :desc).limit(10)
+      @available_projects = Project.order(created_at: :desc)
     end
 
     # POST /invoices
