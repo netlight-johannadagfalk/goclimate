56a6abaa60e19c33e87fb33b3c09bcbd70bdf761
e-ry <erya@netlight.com>
2021-06-29 13:19:18 +0200

    Basic Container component and nesting using Title component

---
+++ app/assets/main/react_components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './Title.jsx';
+
+const Container = (props) => {
+  
+  return (
+    <div className="callout">
+      <Title question = {"hej"}/>
+    </div>
+  )
+}
+
+export default Container;
---
+++ app/assets/main/react_components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = (props) => {
+    
+  return (
+    <h2 className="heading my-4" > 
+      {props.question}
+    </h2>
+  )
+}
+
+export default Title;
