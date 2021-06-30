b2d3b04cc43b69ddc6b215ae41628e930b327e3d
Johanna Dagfalk <jdag@c02f5a5yml7h.sto.netlight.se>
2021-06-30 13:18:41 +0200

    Improved visuals for a better experience

--- app/assets/main/react_components/User/ChecklistItem.jsx
+++ app/assets/main/react_components/User/ChecklistItem.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 const ChecklistItem = ({ challenge, ifChecked }) => {
 
     return (
-        <div className="flex flex-col t:flex-row t:space-x-8 space-y-6 t:space-y-0" onDoubleClick={() => ifChecked(challenge.id)}>
+        <div className="text-left" onClick={() => ifChecked(challenge.id)}>
             <form>
-                <div className={` ${challenge.accepted ? 'callout space-y-6 ml-lg border-8' : 'callout space-y-6 ml-lg'}`}>
-                    <label className="space-x-6 text-xl font-bold">
-                        <input type="checkbox" onChange={() => ifChecked(challenge.id)} />
+                <div className={` ${challenge.accepted ? 'callout border-8 max-w-2xl' : 'callout max-w-2xl '}`}>
+                    <label className="space-x-6 text-l font-bold">
+
                         {' ' + challenge.text}
-                        {' ' + challenge.emoji}
                     </label>
+                    <p className="float-right">{challenge.emoji}</p>
                 </div>
             </form>
         </div>
