1cd47ad7d3e095f09cbe21057b170f9f2b4471b5
Emma Bäckström <emmabckstrm@users.noreply.github.com>
2021-07-01 13:35:03 +0200

    Remove choice from price increase page (#525)

--- config/routes.rb
+++ config/routes.rb
@@ -60,13 +60,7 @@ Rails.application.routes.draw do
     resource :transparency, controller: 'about/transparency', only: [:show]
     resource :privacy_policy, controller: 'about/privacy_policy', path: 'privacy-policy', only: [:show]
     resource :travel_calculator, controller: 'travel_calculator', path: 'travel-calculator', only: [:show]
-    resource :price_increase_confirmation, controller: 'price_increase_confirmation', path: 'price-increase', only: [:new, :create], path_names: { new: '' } do
-      member do
-        post :opt_in
-        post :opt_out
-        get 'thank-you'
-      end
-    end
+    resource :price_increase_confirmation, controller: 'price_increase_confirmation', path: 'price-increase', only: [:show], path_names: { new: '' }
     resource :climate_tips, path: 'climate-tips', only: [:show]
     resource :take_action, controller: 'take_action', path: 'take-action', only: [:show]
 
