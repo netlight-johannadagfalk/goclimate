3dd9dd65a2ec4cf99a20b16f47733ffda965e437
Ebba Nilsson <ebni@c02f539bml7h.sto.netlight.se>
2021-07-02 10:24:57 +0200

    add get data from db in checklist to show existing actions

--- app/assets/main/react_components/User/ClimateActionContainer.jsx
+++ app/assets/main/react_components/User/ClimateActionContainer.jsx
@@ -5,52 +5,9 @@ import Checklist from './Checklist.jsx'
 import ChecklistHeader from './ChecklistHeader.jsx'
 import ChecklistFooter from './ChecklistFooter.jsx'
 
-const ClimateActionContainer = () => {
-    const [climateActions, setClimateActions] = useState([
-        {
-            id: 1,
-            text: 'Go Vegan',
-            category: 'Food',
-            emoji: '🥗',
-            accepted: false,
-        },
-        {
-            id: 2,
-            text: 'No flying',
-            category: 'Transport',
-            emoji: '🛬',
-            accpeted: false,
-        },
-        {
-            id: 3,
-            text: 'Take the bike',
-            category: 'Transport',
-            emoji: '🚲',
-            accpeted: false,
-        },
-        {
-            id: 4,
-            text: 'Contact politician',
-            category: 'Politics',
-            emoji: '🏛',
-            accpeted: false,
-        },
-        {
-            id: 5,
-            text: 'Demonstrate',
-            category: 'Politics',
-            emoji: '🏛',
-            accpeted: false,
-        },
-        {
-            id: 6,
-            text: 'Veggie month!',
-            category: 'Food',
-            emoji: '🥗',
-            accpeted: false,
-
-        },
-    ])
+const ClimateActionContainer = ({climateActionsProps}) => {
+    const [climateActions, setClimateActions] = useState(JSON.parse(climateActionsProps))
+    
 
     const onCheck = (id) => {
         setClimateActions(climateActions.map((climateAction) => climateAction.id === id ? { ...climateAction, accepted: !climateAction.accepted } : climateAction))
@@ -61,7 +18,7 @@ const ClimateActionContainer = () => {
             <div className="max-w-5xl mx-auto space-y-3 t:bg-white t:rounded-lg t:shadow-lg t:p-8 t:border t:border-gray-tint-2">
                 <ChecklistHeader />
                 <div className="flex flex-col t:flex-row t:space-x-8 space-y-6 t:space-y-0">
-                    <div className="t:w-1/2 space-y-6"><Checklist climateActions={climateActions} onCheck={onCheck} isOnlyAccepted={false} />
+                  <div className="t:w-1/2 space-y-6"><Checklist climateActions={climateActions} onCheck={onCheck} isOnlyAccepted={false} />
                     </div>
                     <div className="t:w-1/2 space-y-6">
                         <Checklist climateActions={climateActions} onCheck={onCheck} isOnlyAccepted={true} />
