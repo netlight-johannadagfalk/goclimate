35b4023f25760c5027af1a5d2e0d4fdb176e0020
Elina Olsson <elina.olsson@netlight.com>
2021-07-07 17:13:08 +0200

    Added grid on action cards for styling

--- app/assets/main/react_components/User/CarouselActionItem.jsx
+++ app/assets/main/react_components/User/CarouselActionItem.jsx
@@ -8,11 +8,19 @@ const CarouselActionItem= ({ action, setStatus }) => {
     }
 
     return (
-        <div className="callout m-2 p-2" style={{height:'350px'}}>
-            <h3 className="text-base font-bold" style={{color: 'rgba(28, 70, 55, var(--tw-text-opacity))'}}>{action.name}</h3>
-            <p>{action.description}</p>
-            <img src='earth.png' width="50%"></img>
-            <button className="button button-cta inline-block align-bottom" onClick={() => handleClick(action)}>Accept challenge</button>
+        <div className="callout m-2 p-2 grid grid-flow-col grid-cols-1 grid-rows-4 gap-1" style={{height:'350px'}}>
+            <div className="h-1">
+                <h3 className="text-base font-bold" style={{color: 'rgba(28, 70, 55, var(--tw-text-opacity))'}}>{action.name}</h3>
+            </div>
+            <div>
+                <p className="align-bottom">{action.description}</p>
+            </div>
+            <div>
+                <img src='earth.png' width="50%"></img>
+            </div>
+            <div>
+                <button className="button button-cta inline-block align-bottom" onClick={() => handleClick(action)}>Accept challenge</button>
+            </div>
         </div>
     );
 }
