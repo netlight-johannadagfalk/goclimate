1c783bb847fca613770b8c907c7eb94b242e7ab4
Madeleine Bäckström <maba@netlight.com>
2021-08-10 08:41:12 +0200

    Add input validation control for car and flight questions, only allowing integers and floats

--- app/assets/main/react_components/footprint_form/OptionNumerical.jsx
+++ app/assets/main/react_components/footprint_form/OptionNumerical.jsx
@@ -2,15 +2,24 @@ import React, { useState }  from 'react';
 import AnswerButton from './AnswerButton.jsx';
 
 const OptionNumerical = ({ onAnswerGiven, isCarOption }) => {
-  const [value, setValue] = useState(-1);
+  const [value, setValue] = useState("");
 
   return (
     <div className="flex flex-col m-lg:flex-row" >
       <label className="input mb-3 m-lg:mb-0 m-lg:mr-3 flex">
-        <input key={isCarOption ? "car" : "flight"} autoFocus type="number" min="0" pattern="[0-9]*$" max="2147483647" size="7" className="flex-1" onChange={e => setValue(e.target.value)}/>
+        <input key={isCarOption ? "car" : "flight"} autoFocus type="text" min="0" pattern="[0-9]+[.,]?[0-9]*" max="2147483647" size="7" className="flex-1" value={value} 
+          onChange={(e) => {
+            if(e.target.validity.valid)
+              setValue(e.target.value);
+          }}/>
         {isCarOption ? <span className="ml-3">km</span> : <></>}
       </label>
-      <AnswerButton onAnswerGiven={() => onAnswerGiven(value)} labelText="Next"/>
+      <AnswerButton labelText="Next"
+        onAnswerGiven={() => {
+          setValue("");
+          onAnswerGiven(value.replace(/[,.]\s*$/, ""));
+        }}
+      />
     </div>
   )
 }
