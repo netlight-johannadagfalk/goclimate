5f569ec6a704308e1fc8b9cf9aedeafb99fb74a1
Madeleine Bäckström <maba@netlight.com>
2021-08-12 16:58:53 +0200

    Simplify lifestyleFootprintsText

--- app/assets/main/react_components/footprint_form/Container.jsx
+++ app/assets/main/react_components/footprint_form/Container.jsx
@@ -6,8 +6,8 @@ const Container = ({ footprint, calculator, lifestyleFootprintsText, lang }) => 
   return (
       <div className="callout">
           <FootprintForm 
-            questions={JSON.parse(lifestyleFootprintsText)[lang].views.lifestyle_footprints.questions} 
-            options={JSON.parse(lifestyleFootprintsText)[lang].views.lifestyle_footprints.options} 
+            questions={JSON.parse(lifestyleFootprintsText).lifestyle_footprints.questions} 
+            options={JSON.parse(lifestyleFootprintsText).lifestyle_footprints.options} 
             footprint={JSON.parse(footprint)} 
             calculator={JSON.parse(calculator)}
           />
