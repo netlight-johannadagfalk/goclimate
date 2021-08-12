4a2bdfac61fb5e89164805d50b8c2e5b7d1515ef
Teodor Lennmark <tele@netlight.com>
2021-08-12 08:39:12 +0200

    Integrated function handle Enter key press in the return statement

--- app/assets/main/react_components/footprint_form/OptionNumerical.jsx
+++ app/assets/main/react_components/footprint_form/OptionNumerical.jsx
@@ -4,18 +4,17 @@ import AnswerButton from './AnswerButton.jsx';
 const OptionNumerical = ({ onAnswerGiven, isCarOption }) => {
   const [value, setValue] = useState("");
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      setValue("");
-      /* .replace() makes sure that trailing commas (, or .) are removed */
-      onAnswerGiven(value.replace(/[,.]\s*$/, ""));
-    }
-  }
-
   return (
     <div className="flex flex-col m-lg:flex-row" >
       <label className="input mb-3 m-lg:mb-0 m-lg:mr-3 flex">
-        <input autoFocus type="text" min="0" pattern="[0-9]+[.,]?[0-9]*" max="2147483647" size="7" className="flex-1" value={value} onKeyPress={handleKeyDown} 
+        <input autoFocus type="text" min="0" pattern="[0-9]+[.,]?[0-9]*" max="2147483647" size="7" className="flex-1" value={value}
+          onKeyPress = {(event) => {
+            if (event.key === 'Enter') {
+              setValue("");
+              /* .replace() makes sure that trailing commas (, or .) are removed */
+              onAnswerGiven(value.replace(/[,.]\s*$/, ""));
+            }
+          }} 
           onChange={(e) => {
             /* Checks if input is valid compared to set requirements */
             if(e.target.validity.valid)
