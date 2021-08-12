5fc0fda3c1f1daedbb713c9e1aebbe40931a04e9
Madeleine Bäckström <maba@netlight.com>
2021-08-12 09:51:34 +0200

    Code cleanup, localization version 1 done

--- app/assets/main/react_components/footprint_country_select/CountryForm.jsx
+++ app/assets/main/react_components/footprint_country_select/CountryForm.jsx
@@ -4,17 +4,17 @@ import countryList from 'react-select-country-list'
 /**
  * Form for choosing country of residence prior to filling out footprint calculator
  */
-const CountryForm = ({ data, shared, lang }) => {
+const CountryForm = ({ lang, data }) => {
 
     return (
-        <form className="m-lg:flex m-lg:justify-center mt-3" action={lang !== 'en' ? lang === 'sv' ? 'se/calculator' : lang + '/calculator' : '/calculator'}>
+        <form className="m-lg:flex m-lg:justify-center mt-3" action={lang === 'en' ? '/calculator' : lang === 'sv' ? 'se/calculator' : lang + '/calculator'}>
             <div className="select-wrapper w-full m-lg:w-auto m-lg:max-w-xs">
-                <label htmlFor="country" className="sr-only">{shared.select_country}</label>
+                <label htmlFor="country" className="sr-only">{data.select_country}</label>
                 <select skip_default_ids="false" allow_method_names_outside_object="true" className="select" required="required" name="country" id="country">
-                <option value="">{data.i_live_in}</option>
-                {
-                    useMemo(() => countryList().getData(), []).map((country) => <option value={country.value} key={country.value}>{country.label}</option>)
-                }
+                    <option value="">{data.i_live_in}</option>
+                    {
+                        useMemo(() => countryList().getData(), []).map((country) => <option value={country.value} key={country.value}>{country.label}</option>)
+                    }
                 </select>
             </div>
             <input type="submit" value="Get started" className="button button-cta mt-2 m-lg:mt-0 m-lg:ml-2 w-full m-lg:w-auto" data-disable-with="Get started"/>
--- app/assets/main/react_components/footprint_country_select/CountrySelectContainer.jsx
+++ app/assets/main/react_components/footprint_country_select/CountrySelectContainer.jsx
@@ -4,13 +4,13 @@ import CountryForm from "./CountryForm.jsx";
 /**
  * Main component for React components on start page 
  */
-const CountrySelectContainer = ({ lang, data, shared }) => {
+const CountrySelectContainer = ({ lang, welcome, shared }) => {
 
     return (
       <>
-          <h1 className="heading-xl mb-6 t:mb-12">{JSON.parse(data).hero.heading}</h1>
-          <p className="my-4 max-w-lg mx-auto">{JSON.parse(data).hero.body_text}</p>
-          <CountryForm lang={lang} data={JSON.parse(data)} shared={JSON.parse(shared)} />
+          <h1 className="heading-xl mb-6 t:mb-12">{JSON.parse(welcome).hero.heading}</h1>
+          <p className="my-4 max-w-lg mx-auto">{JSON.parse(welcome).hero.body_text}</p>
+          <CountryForm lang={lang} data={{...JSON.parse(welcome), ...JSON.parse(shared)}} />
       </>
     );
 };
