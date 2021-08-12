e6fa63369e4fe11317ec516405b2b3be1e9555fd
Teodor Lennmark <tele@netlight.com>
2021-08-12 15:41:57 +0200

    Added a div around the form making it posible to insert content outside of the white form area

--- app/assets/main/react_components/footprint_form/Container.jsx
+++ app/assets/main/react_components/footprint_form/Container.jsx
@@ -4,15 +4,20 @@ import FootprintForm from './FootprintForm.jsx';
 
 const Container = ({ footprint, calculator }) => {
   return (
-      <div className="callout">
-          <FootprintForm 
-            questions={Questions.en.views.lifestyle_footprints.questions} 
-            options={Questions.en.views.lifestyle_footprints.options} 
-            footprint={JSON.parse(footprint)} 
-            calculator={JSON.parse(calculator)}
-          />
+      <div className="space-y-6"> 
+        <div className="callout">
+            <FootprintForm 
+              questions={Questions.en.views.lifestyle_footprints.questions} 
+              options={Questions.en.views.lifestyle_footprints.options} 
+              footprint={JSON.parse(footprint)} 
+              calculator={JSON.parse(calculator)}
+            />
+        </div>
+          <div class="text-sm">
+          <a class="link" href="https://www.goclimate.com/blog/methodology-behind-the-carbon-footprint-calculator/" target="_blank">How do we calculate?</a>
+          </div>
       </div>
-  )
+  )   
 }
 
 export default Container;
