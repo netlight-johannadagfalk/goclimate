af0b0a20ec69ff85068722dee2c824426d66221f
Madeleine Bäckström <maba@netlight.com>
2021-08-13 08:23:25 +0200

    Create React result components container

---
+++ app/assets/main/react_components/footprint_result/ResultContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/**
+ * React container for Result page components
+ */
+const ResultContainer = () => {
+
+    return (
+        <div className="pr-16">
+            <div className="relative pb-1">
+                <div className="space-y-6">
+                    { /* React components */ }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ResultContainer;
