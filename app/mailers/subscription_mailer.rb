b68f70f69821fd13e441fda1e2f23bdf6d00e8e6
Sara Andersson <sara.andersson@netlight.com>
2021-08-13 13:33:42 +0200

    added user actions to the action of the month mail so the user easily can see the current actions

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -14,6 +14,13 @@ class SubscriptionMailer < ApplicationMailer
     @total_carbon_offset = OffsettingStatistics.new.total_sold.tonnes.round
     @action_of_the_month = ClimateAction.where(action_of_the_month: true).select("*").first
     @user_climate_actions = UserClimateAction.where(id: @user.id).select("*")
+    if @user_climate_actions.nil?
+      @user_climate_actions = UserClimateAction.where(id: @user.id).select("*")
+    else
+      @user_climate_actions = ClimateAction.joins(:user_climate_actions)
+      .where(["user_climate_actions.user_id = ? and climate_actions.id = user_climate_actions.climate_action_id", @user.id])
+      .select(:name, :description, :status, :points)
+    end
     mail subject: I18n.t('thank_you')
   end
 
