a2bda46560c5ba4b04b002ee4510b6a10bbc388d
Teodor Lennmark <tele@netlight.com>
2021-08-16 18:30:55 +0200

    Clean up

--- app/assets/main/react_components/footprint_result/MembershipTypeChooser.jsx
+++ app/assets/main/react_components/footprint_result/MembershipTypeChooser.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import SelectorMultipleTimes from './SelectorMultipleTimes.jsx';
 import SelectButton from './SelectButton.jsx';
 
 /**
@@ -11,9 +10,9 @@ const MembershipTypeChooser = () => {
     return (
         <form className="space-y-3" data-target="registrations--price.peopleForm" data-url="/users/sign_up" action="/users/sign_up" accept-charset="UTF-8" method="get">
             <input value="b3ec26a402c164dd2f8a78bf7e95841922cbb9bc" type="hidden" name="lifestyle_footprint" id="lifestyle_footprint"/>
-            <SelectButton selectedMembership={selectedMembership} setSelectedMembership={setSelectedMembership} buttonType="free" preChecked={false} boldDisplayedText="Free account." unBoldDisplayedText="Track your carbon footprint over time." />
-            <SelectButton selectedMembership={selectedMembership} setSelectedMembership={setSelectedMembership} buttonType="single" preChecked={true} boldDisplayedText="Offset" unBoldDisplayedText=" your carbon footprint through climate projects."/>
-            <SelectButton selectedMembership={selectedMembership} setSelectedMembership={setSelectedMembership} buttonType="multi" preChecked={false} boldDisplayedText="Offset multiple times." unBoldDisplayedText="Offset more for yourself or for loved ones."/>
+            <SelectButton selectedMembership={selectedMembership} setSelectedMembership={setSelectedMembership} buttonType="free" boldDisplayedText="Free account." unBoldDisplayedText="Track your carbon footprint over time." />
+            <SelectButton selectedMembership={selectedMembership} setSelectedMembership={setSelectedMembership} buttonType="single" boldDisplayedText="Offset" unBoldDisplayedText=" your carbon footprint through climate projects."/>
+            <SelectButton selectedMembership={selectedMembership} setSelectedMembership={setSelectedMembership} buttonType="multi" boldDisplayedText="Offset multiple times." unBoldDisplayedText="Offset more for yourself or for loved ones."/>
         </form>
     )
 }
--- app/assets/main/react_components/footprint_result/SelectButton.jsx
+++ app/assets/main/react_components/footprint_result/SelectButton.jsx
@@ -2,19 +2,21 @@ import React, { useState }  from 'react';
 import SelectorMultipleTimes from './SelectorMultipleTimes.jsx';
 
 /**
- * React container for Result page components
+ * React container for select puttons 
  */
-const SelectButton = ({selectedMembership, setSelectedMembership, buttonType, preChecked, boldDisplayedText, unBoldDisplayedText}) => {
+const SelectButton = ({selectedMembership, setSelectedMembership, buttonType, boldDisplayedText, unBoldDisplayedText}) => {
 
     return (
         <label className="flex flex-row items-center p-3 bg-gray-pastel rounded cursor-pointer" 
             data-active-class="flex flex-row items-center p-3 bg-green-tint-1 rounded cursor-pointer" 
             data-inactive-class="flex flex-row items-center p-3 bg-gray-pastel rounded cursor-pointer" 
             htmlFor={buttonType}>
-             <input className="flex-shrink-0 mr-2" type="radio" name="membership" id={buttonType} checked = {selectedMembership === buttonType} value={buttonType} onChange={() => setSelectedMembership(buttonType)}
+            <input className="flex-shrink-0 mr-2" type="radio" name="membership" id={buttonType} 
+            checked = {selectedMembership === buttonType} 
+            value={buttonType} 
+            onChange={() => setSelectedMembership(buttonType)}
             data-target="registrations--membership-choice.choice" 
             data-action="click->registrations--membership-choice#handleChange"/>        
-            
             <span><span className="font-bold">{boldDisplayedText}</span> {unBoldDisplayedText}</span>
             { buttonType == "multi" ?
                     <SelectorMultipleTimes/>
