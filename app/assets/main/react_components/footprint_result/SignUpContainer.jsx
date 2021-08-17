9393f7741902cb6d595e059bcd4dd8d34cc6ad89
Teodor Lennmark <tele@netlight.com>
2021-08-17 10:46:58 +0200

    Added wrapping arround component

--- app/assets/main/react_components/footprint_result/SignUpContainer.jsx
+++ app/assets/main/react_components/footprint_result/SignUpContainer.jsx
@@ -8,9 +8,29 @@ const SignUpContainer = () => {
     console.log("SignUpContainer render")
 
     return (
-        <div className="pr-16">
-            <MembershipTypeChooser/>
+        <div className="relative pb-1">
+            <div className="space-y-6">
+                <div className="callout max-w-md mx-auto" data-controller="registrations--form registrations--membership-choice">
+                    <a id="sign-up" className="absolute -mt-32"></a>
+                    <div className="space-y-3" data-controller="registrations--price registrations--referral-code" data-target="registrations--membership-choice.priceControllerElement">
+                        <h2 className="text-lg d:text-xl font-semibold text-center">
+                            {/* Add translate functionality here */}
+                            Sign up now! If we all contribute we can reach the goal together!
+                        </h2>
+                        <p className="text-sm text-center mb-6 hidden"> {/* Add translate functionality here */}Step 1/2</p>
+                        <p className="text-center"> {/* Add translate functionality here */} Sign up to save your result, get support in reducing your footprint, and opt in to carbon offset.</p>
+                        <input type="checkbox" id="step-one-done" className="toggler"/>
+                        <div className="toggler-checked:hidden">
+                            <div className="space-y-3">
+                                <div className="h-px w-full bg-gray-tint-2 hidden"></div>
+                                <MembershipTypeChooser/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
