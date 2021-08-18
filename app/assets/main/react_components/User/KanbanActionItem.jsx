e5a31e4d25621726e2aded753d3199ecbaa33fff
Elina Olsson <elina.olsson@netlight.com>
2021-08-18 09:43:07 +0200

    Card is expandable so users can click to see more info

--- app/assets/main/react_components/User/KanbanActionItem.jsx
+++ app/assets/main/react_components/User/KanbanActionItem.jsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
 
 const KanbanActionItem = ({ item, index, handleDelete }) => {
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpand = (item) => {
+    console.log(item)
+    console.log(expanded)
+    setExpanded(
+      
+    )
+  }
+
   return (
     <Draggable key={item.id} draggableId={item.id} index={index}>
       {(provided) => {
         return (
+          expanded ? (
           <div
+            className="callout space-x-6 text-l font-bold"
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            style={{
+              userSelect: "none",
+              padding: 16,
+              margin: "0 0 8px 0",
+              minHeight: "auto",
+              ...provided.draggableProps.style,
+            }}
+          >
+            {item.name}
+            <button
+                className="float-right ml-4 fas fa-chevron-up"
+                onClick={() => handleExpand(item)}
+              ></button>
+            {item.status === false && (
+              <button
+                className="float-right fas fa-trash"
+                onClick={() => handleDelete(item.id, item.climate_action_id)}
+              ></button>
+            )}
+
+              <div className="text-s ml-0 flex inherit"
+              style={{
+                margin: "0",
+                fontWeight: "normal",
+                fontSize: "16px",
+              }}>
+                <p>{item.description}</p>
+              </div>
+            
+
+
+          </div>) : (
+            <div
             className="callout space-x-6 text-l font-bold"
             ref={provided.innerRef}
             {...provided.draggableProps}
@@ -20,6 +69,10 @@ const KanbanActionItem = ({ item, index, handleDelete }) => {
             }}
           >
             {item.name}
+            <button
+                className="float-right ml-4 fas fa-chevron-down"
+                onClick={() => setExpanded(!expanded)}
+              ></button>
             {item.status === false && (
               <button
                 className="float-right fas fa-trash"
@@ -27,6 +80,8 @@ const KanbanActionItem = ({ item, index, handleDelete }) => {
               ></button>
             )}
           </div>
+
+          )
         );
       }}
     </Draggable>
