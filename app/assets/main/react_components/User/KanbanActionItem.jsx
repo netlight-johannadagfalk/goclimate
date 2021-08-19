41494bf5ad75e2944672163b5eee25ba57e82d5b
johanna <johanna.dagfalk@netlight.com>
2021-08-19 16:04:18 +0200

    Actionpoints on items in carusell and aligned buttons in kanban items

--- app/assets/main/react_components/User/KanbanActionItem.jsx
+++ app/assets/main/react_components/User/KanbanActionItem.jsx
@@ -9,7 +9,7 @@ const KanbanActionItem = ({ item, index, handleDelete, handlePerformance }) => {
       {(provided) => {
         return (
           <div
-            className="callout space-x-6 text-l font-bold"
+            className="callout grid grid-cols-3 gap-4 items-baseline space-x-6 text-l font-bold"
             ref={provided.innerRef}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
@@ -21,36 +21,36 @@ const KanbanActionItem = ({ item, index, handleDelete, handlePerformance }) => {
               ...provided.draggableProps.style,
             }}
           >
-            {item.name}
+            <div>{item.name}</div>
             {item.status === false ? (
-              <div>
+              <div className="col-end-auto col-span-2 ">
                 <button
-                  className={`float-right ml-4 fas ${
-                    expanded ? 'fa-chevron-up' : 'fa-chevron-down'
-                  }`}
-                  onClick={() => setExpanded(!expanded)}
-                ></button>
-                <button
-                  className="float-right fas fa-trash"
-                  onClick={() => handleDelete(item.id, item.climate_action_id)}
-                ></button>
-                <button
-                  className="button button-cta"
+                  className="button ml-4 button-cta col-end-auto "
                   onClick={() => handlePerformance(item, true)}
                 >
                   Performed{' >'}
                 </button>
+                <button
+                  className=" ml-4 fas fa-trash col-end-auto "
+                  onClick={() => handleDelete(item.id, item.climate_action_id)}
+                ></button>
+                <button
+                  className={`ml-4 fas col-end-auto ${
+                    expanded ? 'fa-chevron-up' : 'fa-chevron-down'
+                  }`}
+                  onClick={() => setExpanded(!expanded)}
+                ></button>
               </div>
             ) : (
-              <div>
+              <div className="col-span-2  ">
                 <button
-                  className="button button-cta"
+                  className="button button-cta  "
                   onClick={() => handlePerformance(item, false)}
                 >
                   Unperform{' <'}
                 </button>
                 <button
-                  className={`float-right ml-4 fas ${
+                  className={`ml-4 fas  ${
                     expanded ? 'fa-chevron-up' : 'fa-chevron-down'
                   }`}
                   onClick={() => setExpanded(!expanded)}
@@ -60,14 +60,23 @@ const KanbanActionItem = ({ item, index, handleDelete, handlePerformance }) => {
 
             {expanded && (
               <div
-                className="text-s ml-0 flex inherit"
+                className="text-s ml-0  inherit"
                 style={{
                   margin: '0',
                   fontWeight: 'normal',
                   fontSize: '16px',
                 }}
               >
-                <p>{item.description}</p>
+                <div className="flex-1">
+                  <p>
+                    {item.description.length > 40
+                      ? item.description.slice(0, 40) + '...'
+                      : item.description}
+                  </p>
+                </div>
+                <div className="flex-1">
+                  <img src="earth.png" className=" flex"></img>
+                </div>
               </div>
             )}
           </div>
