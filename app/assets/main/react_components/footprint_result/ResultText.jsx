17f55bc9cd6bbe09e2f4e70aee287e23b89c4276
Madeleine Bäckström <maba@netlight.com>
2021-08-20 12:38:17 +0200

    Component comment

--- app/assets/main/react_components/footprint_result/ResultText.jsx
+++ app/assets/main/react_components/footprint_result/ResultText.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+/**
+ * Simple text component that can be modified using the customValues array
+ * If customValues has values, each value is placed in the first found spot in the text that matches: %{...}
+ */
 const ResultText = ({text, customValues = []}) => {
 
     const findCustomPlacement = /%{.*?}/i
