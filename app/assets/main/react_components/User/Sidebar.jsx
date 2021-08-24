661dd288cf576c988c7845ee08d86a806745c3b3
Elina Olsson <elina.olsson@netlight.com>
2021-08-24 13:37:30 +0200

    Placed sidebar behind header and footer so it wont cover any important content

--- app/assets/main/react_components/User/Sidebar.jsx
+++ app/assets/main/react_components/User/Sidebar.jsx
@@ -4,29 +4,27 @@ const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <div>
+    <div
+      className={`bg-white h-screen right-0 top-20 fixed z-40 border-l border-gray-accent ${
+        collapsed ? "w-10" : "w-1/6"
+      }`}
+    >
+      <button
+        className={`fas static m-2 top-0 right-10 text-l ${
+          collapsed ? "fa-arrow-alt-circle-left" : "fa-arrow-alt-circle-right"
+        }`}
+        onClick={() => setCollapsed(!collapsed)}
+      ></button>
       <div
-        className={`bg-white h-screen right-0 top-20 fixed z-200 border-l-2 border-gray-accent ${
-          collapsed ? "w-10" : "w-80"
+        className={`flex flex-col h-full justify-center ${
+          collapsed ? "invisible" : "visible"
         }`}
       >
-        <button
-          className={`fas static m-2 top-0 right-10 text-xl z-100 ${
-            collapsed ? "fa-arrow-alt-circle-left" : "fa-arrow-alt-circle-right"
-          }`}
-          onClick={() => setCollapsed(!collapsed)}
-        ></button>
-        <div
-          className={`flex flex-col h-full justify-center ${
-            collapsed ? "invisible" : "visible"
-          }`}
-        >
-          <div className="flex-1 h-full justify-center">
-            <h3 className="heading text-center">My Actions</h3>
-          </div>
-          <div className="flex-1 h-full justify-center border-t-2 border-gray-accent pt-5">
-            <h3 className="heading text-center">My Badges</h3>
-          </div>
+        <div className="flex-1 h-full justify-center">
+          <h4 className="text-base font-bold text-center">My Actions</h4>
+        </div>
+        <div className="flex-1 h-full justify-center border-t border-gray-accent pt-5">
+          <h4 className="text-base font-bold text-center">My Badges</h4>
         </div>
       </div>
     </div>
