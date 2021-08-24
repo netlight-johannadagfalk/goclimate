218f6fff8ca61a0ffe6ad94f743ce09c2b6619cc
Teodor Lennmark <tele@netlight.com>
2021-08-24 16:51:51 +0200

    Separate function for handeling price calculation

--- app/assets/main/react_components/footprint_result/PriceText.jsx
+++ app/assets/main/react_components/footprint_result/PriceText.jsx
@@ -4,12 +4,46 @@ import React from 'react'
  * Title used on footprint result page
  */
 const PriceText = ({price}) => {
+
+  function extractPrice (price) {
+
+    console.log((Number.isInteger(price.subunit_amount/100)))
+    if (Number.isInteger(price.subunit_amount/100)) {
+      return (price.subunit_amount/100)
+    } else {
+      return (price.subunit_amount/100)
+    }
+
+    // def to_s(precision: nil) # rubocop:disable Metrics/MethodLength TODO: This needs simplifying
+    //   formatting_options = { format: '%n' }
+    //   unless precision.nil?
+    //     formatting_options[:precision] =
+    //       if precision == :auto
+    //         subunit_amount % 100 == 0 ? 0 : 2
+    //       else
+    //         precision
+    //       end
+    //   end
+    //   formatted_number = ActiveSupport::NumberHelper.number_to_currency(amount, formatting_options)
+
+    //   localized_string = I18n.t(
+    //     "models.money.currency_formats.#{currency.iso_code}",
+    //     number: formatted_number, default: 'DEFAULT', fallback: false
+    //   )
+
+    //   if localized_string == 'DEFAULT'
+    //     "#{currency} #{formatted_number}"
+    //   else
+    //     localized_string
+    //   end
+    // end
+  }
     
     return (
         <div className="py-6 space-y-1">
             <p className="heading-lg text-center">
               <span className="hidden">First month free</span>
-              <span><span>€7---  </span>/month</span>
+              <span><span>€7--- {extractPrice (price)}  </span>/month</span>
               {/* @plan.price.to_s(precision: :auto) */}
             </p>
             <p className="hidden">
