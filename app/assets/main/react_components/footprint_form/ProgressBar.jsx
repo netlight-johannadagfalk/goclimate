cbbfebc28b0f6778d5727049ee5012d090a536cb
Madeleine Bäckström <86283836+mabackstr@users.noreply.github.com>
2021-08-26 12:15:26 +0200

    Separate pages for result and questions, task complete

--- app/assets/main/react_components/footprint_form/ProgressBar.jsx
+++ app/assets/main/react_components/footprint_form/ProgressBar.jsx
@@ -5,48 +5,48 @@ import React from 'react';
  * Dots under each category is taken from each question in questionCategories, originally from footprintform
  */
 const ProgressBar = ({ questionCategories, activeCategory, activeQuestion }) => {
-  const categories = ["home", "utensils", "shopping-bag", "car", "plane"]
+  const categories = ["home", "utensils", "shopping-bag", "car", "plane", "chart-bar"]
   const inactiveCategoryClass = "border-gray-tint-2"
   const activeCategoryClass = "border-green-tint-1 bg-green-tint-1 text-primary"
   const greenDotClass = "fas text-green-accent"
   const grayDotClass = "far text-gray-accent"
   let currentClass = ""
-  let reachedActiveCategory = false
+  let isCompletedCategory = true
 
   return (
-  <>
-    <div className="flex justify-center space-x-3 m-lg:space-x-6 text-gray-shade-2" >
-    {categories.map((category) => {
-      activeCategory == category ? currentClass = activeCategoryClass : currentClass = inactiveCategoryClass 
-      activeCategory == category ? reachedActiveCategory = true : reachedActiveCategory = reachedActiveCategory
-        return (
-          <div key={category}>
-            <div className={"rounded-full w-12 h-12 flex justify-center items-center box-content p-1 mb-2 border " + currentClass}>
-              <div className={"fas fa-lg " + "fa-" + category}></div>
-            </div>
-            {
-            <div className="flex justify-center space-x-1 text-gray-shade-2 content-between text-sm text-gray-accent block">
-              {!reachedActiveCategory && <div className="text-green-accent block"><i className="text-base fa fa-check-circle"></i></div>}
-              {Object.keys(questionCategories).map((question) => 
-              {
-                if(reachedActiveCategory){
-                  if(questionCategories[question] == category){
-                    if(question == activeQuestion){
-                      return(<i key={question} className={"fa-circle text-sm block " + greenDotClass}/>)
+    <>
+      <div className="flex justify-center space-x-3 m-lg:space-x-6 text-gray-shade-2" >
+        {categories.map((category) => {
+          activeCategory == category ? currentClass = activeCategoryClass : currentClass = inactiveCategoryClass 
+          activeCategory == category ? isCompletedCategory = false : isCompletedCategory = isCompletedCategory
+          return (
+              <div key={category}>
+                <div className={"rounded-full w-12 h-12 flex justify-center items-center box-content p-1 mb-2 border " + currentClass}>
+                  <div className={"fas fa-lg " + "fa-" + category}></div>
+                </div>
+                {
+                <div className="flex justify-center space-x-1 text-gray-shade-2 content-between text-sm text-gray-accent block">
+                  {isCompletedCategory && <div className="text-green-accent block"><i className="text-base fa fa-check-circle"></i></div>}
+                  {Object.keys(questionCategories).map((question) => 
+                  {
+                    if(!isCompletedCategory){
+                      if(questionCategories[question] == category){
+                        if(question == activeQuestion){
+                          return(<i key={question} className={"fa-circle text-sm block " + greenDotClass}/>)
+                        }
+                        else{
+                          return(<i key={question} className={"fa-circle text-sm block " + grayDotClass}/>) 
+                        }
+                      }
                     }
-                    else{
-                      return(<i key={question} className={"fa-circle text-sm block " + grayDotClass}/>) 
-                    }
-                  }
+                  })}
+                </div>
                 }
-              })}
-            </div>
-            }
-          </div>
-        )
-    })}
-    </div>
-  </>
+              </div>
+            )
+        })}
+      </div>
+    </>
   )
 }
 
--- app/assets/main/react_components/footprint_form/ResultPage.jsx
+++ app/assets/main/react_components/footprint_form/ResultPage.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import YourFootprintText from '../footprint_result/YourFootprintText.jsx'
-import ResultTitle from '../footprint_result/ResultTitle.jsx'
-import WorldComparisonChart from '../footprint_result/WorldComparisonChart.jsx'
+import WorldResultPage from './WorldResultPage.jsx'
+import AnswerButton from './AnswerButton.jsx';
 
-const ResultPage = ({ result, texts, lang }) => {
+const ResultPage = ({ result, texts, lang, page, onPageChange }) => {
     const footprint = result.footprint;
     const countryAverage = result.country_average;
+
     return (
         <div>
-            <ResultTitle
-                title={texts.registrationsText.well_done}
-            />
-            <YourFootprintText
-                footprintText={{
-                    heading: texts.commonText.dashboard.footprint.heading,
-                    tonnes_CO2: texts.commonText.tonnes_CO2
-                }} 
-                footprintValue={(footprint.total.co2e / 1000).toFixed(1)}
-            />
-            <WorldComparisonChart 
-                footprint={footprint}
-                countryAverage={countryAverage}
-                worldComparisonText={{...texts.registrationsText, ...texts.commonText, ...texts.modelText}}
-                lang={lang}
+            {page === 0 ?
+                <WorldResultPage
+                    footprint={footprint}
+                    countryAverage={countryAverage} 
+                    texts={texts} 
+                    lang={lang}
+                />
+            :
+                <div>
+                    Placeholder: CategoriesResultPage
+                </div>
+            }
+            <AnswerButton
+                label={texts.lifestyleFootprintsText.next + " ->"}
+                onAnswerGiven={onPageChange}
+                stylingClasses={"w-2/3"}
             />
         </div>
     )
