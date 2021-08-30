0292a5d3aed03f14d7f3b7b0cdce37c69ebbb054
Elina Olsson <elina.olsson@netlight.com>
2021-08-30 08:33:34 +0200

    In progress

--- app/assets/main/react_components/User/CarouselCategoryButton.jsx
+++ app/assets/main/react_components/User/CarouselCategoryButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import "././components/category_colors.css";
 
 const CarouselCategoryButton = ({
   categoryName,
@@ -23,13 +24,13 @@ const CarouselCategoryButton = ({
   return (
     <>
       {active ? (
-        <button className="button inline-block focus:outline-none m-1 bg-primary text-white">
+        <button className="button inline-block focus:outline-none bg-primary text-white m-1">
           {" "}
           {categoryName}{" "}
         </button>
       ) : (
         <button
-          className="button inline-block m-1 focus:outline-none"
+          className={`button inline-block m-1 focus:outline-none`}
           onClick={() => handleCategory(categoryID)}
         >
           {" "}
