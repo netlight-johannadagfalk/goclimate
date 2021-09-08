af034bcb45b1e59b002e96395a75fa66d117d197
Madeleine Bäckström <86283836+mabackstr@users.noreply.github.com>
2021-09-08 15:07:15 +0200

    Destructure context text in missed component

--- app/assets/main/react_components/footprint_result/FAQ.jsx
+++ app/assets/main/react_components/footprint_result/FAQ.jsx
@@ -7,6 +7,8 @@ import { useTexts } from '../context/Footprint/TextsContext.js'
  */
 const FAQ = () => {
 
+    const { registrationsText: { faq }, commonText: { faq_questions }} = useTexts()
+
     // Only the FAQ:s specified in questionKeys are used in the FAQ on the result page
     const questionKeys = {
         climate_neutrality: ["q2"],
@@ -16,13 +18,13 @@ const FAQ = () => {
     let filteredQuestions = []
     // Filters all questions, saving the ones for the specified questionKeys in filteredQuestions
     Object.keys(questionKeys).forEach((questionCategory) => {
-        const filteredQuestionsForCategory = Object.entries(useTexts().commonText.faq_questions[questionCategory].questions).filter(([q])=> questionKeys[questionCategory].includes(q))
+        const filteredQuestionsForCategory = Object.entries(faq_questions[questionCategory].questions).filter(([q])=> questionKeys[questionCategory].includes(q))
         filteredQuestionsForCategory.forEach((q) => filteredQuestions.push(q[1]))
     })
 
     return (
         <div className="text-center max-w-2xl mx-auto space-y-3">
-            <h2 className="heading">{useTexts().registrationsText.faq}</h2>
+            <h2 className="heading">{faq}</h2>
             {filteredQuestions.map((question) => {
                 return (
                     <FAQListChild key={question.question} question={question.question} answer={question.answer} />
