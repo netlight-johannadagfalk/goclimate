4956bb924e91e2e9010a61b9dc724185a034071a
Teodor Lennmark <tele@netlight.com>
2021-09-09 16:41:43 +0200

    Manage to set up a test environment, and pass a mock test

---
+++ app/assets/main/react_components/footprint_result/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * Title used on footprint result page
+ */
+const ResultTitle = ({ title }) => {
+    return (
+        <h1 className="font-semibold text-xl t:text-2xl d-md:text-3xl my-5">
+            {title}
+        </h1>
+    )
+}
+
+export default ResultTitle
--- app/assets/main/react_components/footprint_result/ResultTitle.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, fireEvent } from './test-utils.jsx';
-import ResultTitle from './ResultTitle.jsx'
-
-test('my test', async () => {
-    render(<ResultTitle title="MY TITLE"/>)
-    expect(<ResultTitle></ResultTitle>).toBe("MY TITLE")
-  })
-  
