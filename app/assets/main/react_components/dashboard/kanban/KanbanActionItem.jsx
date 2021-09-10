e2bd9bb3050e91704a37d5589f18670e0f02350b
Ebba <ebba.nilsson@netlight.com>
2021-09-10 13:56:13 +0200

    moved progressbar to kanban

--- app/assets/main/react_components/dashboard/kanban/KanbanActionItem.jsx
+++ app/assets/main/react_components/dashboard/kanban/KanbanActionItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ProgressBar from "../ProgressBar.jsx";
+import ProgressBar from "./ProgressBar.jsx";
 
 import { Draggable } from "react-beautiful-dnd";
 
