5a9d85103bf4c8d76b1a93e1509a7510b671146e
e-ry <erya@netlight.com>
2021-09-14 11:15:04 +0200

    last comments

--- app/assets/main/react_components/footprint_form/Title.jsx
+++ app/assets/main/react_components/footprint_form/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({text, custom_style}) => {
   return (
-    <h2 className={"heading my-4 " + ((!(custom_style === undefined)) ? custom_style : "")} >{text}</h2>
+    <h2 className={"heading my-4 " + (custom_style !== undefined ? custom_style : "")} >{text}</h2>
   )
 }
 
--- app/assets/main/react_components/footprint_result/SignUpPreamble.jsx
+++ app/assets/main/react_components/footprint_result/SignUpPreamble.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-/**
- * React container signup motivation text PREAMBLE
- */
 const SignUpPreamble = ({ text }) => {
     return (
         <p className="text-center"> 
