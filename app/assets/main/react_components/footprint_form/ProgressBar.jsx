4338700d537168c608d6b4cdeb28f559a02f6b51
Madeleine Bäckström <86283836+mabackstr@users.noreply.github.com>
2021-09-15 12:17:36 +0200

    Add invisible feature for complete category

--- app/assets/main/react_components/footprint_form/ProgressBar.jsx
+++ app/assets/main/react_components/footprint_form/ProgressBar.jsx
@@ -41,7 +41,7 @@ const ProgressBar = ({ questionCategories, currentObject }) => {
               {
                 <div className="flex justify-center space-x-1 text-gray-shade-2 content-between w-0 h-0 m-xs:w-full m-xs:h-full  text-gray-accent block">
                   {isCompletedCategory ? (
-                    <div className="text-green-accent block">
+                    <div className="text-green-accent invisible m-xs:visible block">
                       <i className="text-base fa fa-check-circle"></i>
                     </div>
                   ) : (
