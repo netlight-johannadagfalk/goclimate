e8ce15022f9c01854a0642bda69f5fba698a8bbb
e-ry <erya@netlight.com>
2021-09-16 07:39:20 +0200

    fixed worldpage and tested

--- app/assets/main/react_components/footprint_form/WorldPage.jsx
+++ app/assets/main/react_components/footprint_form/WorldPage.jsx
@@ -23,32 +23,6 @@ const WorldPage = ({ footprint, countryAverage, priceObject }) => {
       />
     </div>
   );
-};
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    return (
-        <div className="max-w-lg mx-auto">
-            <Title 
-                text={well_done}
-            />
-            <YourFootprintText
-                footprintValue={(footprint.total.co2e / 1000).toFixed(1)}
-                priceObject={priceObject}
-            />
-            <br></br>
-            <WorldComparisonChart 
-                footprint={footprint}
-                countryAverage={countryAverage}
-            />
-        </div>
-    )
 }
 
 export default WorldPage
-=======
-export default WorldPage;
->>>>>>> 3696c2e151cc28aec1007a5d10b4404b1fe308bb
-=======
-export default WorldPage;
->>>>>>> 7028680ecea761bd979cbd828a8042e07922b244
