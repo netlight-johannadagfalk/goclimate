4b8427a06f92a91bdafadc3de1f3226fb70aac29
Teodor Lennmark <tele@netlight.com>
2021-09-17 14:03:06 +0200

    Start setup

--- app/assets/main/react_components/footprint_result/LatestProjectsList.jsx
+++ app/assets/main/react_components/footprint_result/LatestProjectsList.jsx
@@ -9,11 +9,34 @@ const LatestProjectsList = () => {
   const projects = useProjects();
 
   return (
+    <>
     <div className="space-y-3 text-left">
       <h3 className="font-semibold">{latest_projects}</h3>
-      <ul className="space-y-3">
+      <div className="mt-12 flex flex-col d:flex-row">
+
         {projects.map((project) => (
-          <li
+          <>
+            <div className="d:w-1/3 pt-20 px-1 m-lg:pt-16">
+              <div className="callout h-full space-y-3 pt-0 p-2">
+                <div className="inline-block w-full">
+                  <img
+                    className="mx-auto h-10 w-10 -mt-10 m-lg:h-24 m-lg:w-24 m-lg:-mt-12 rounded-full object-cover"
+                    src={project.image_url}
+                  />
+                </div>
+                <h3 className="font-semibold">
+                  {project.name}
+                </h3>
+                <p>
+                  {project.short_description}
+                </p>
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+
+      {/* <li
             key={project.name}
             className="flex items-center p-3 bg-white rounded border border-gray-tint-2"
           >
@@ -27,10 +50,49 @@ const LatestProjectsList = () => {
               <p className="font-semibold">{project.name}</p>
               <p>{project.country}</p>
             </span>
-          </li>
-        ))}
-      </ul>
+          </li> */}
+      {/* <div className="mt-12 flex flex-col d:flex-row section-gutter">
+
+        <div className="d:w-1/3 pt-20 m-lg:pt-24">
+          <div className="callout h-full space-y-3 pt-0">
+            <div className="inline-block w-full">
+              <img
+                className="mx-auto h-40 w-40 -mt-20 m-lg:h-48 m-lg:w-48 m-lg:-mt-24 rounded-full object-cover"
+                src="https://www.goclimate.com/blog/wp-content/uploads/2017/05/0000213_trang-palm-oil-wastewater-treatment-project-in-trang-province-thailand_550.jpeg"
+              />
+            </div>
+            <h3 className="heading">
+              Trang Palm Oil Wastewater Treatment Project in Trang Province
+            </h3>
+            <p>
+              We have now made our third community climate offset, this time in
+              Trang Palm Oil Wastewater Treatment Project in Trang Province,
+              Thailand.
+            </p>
+          </div>
+        </div>
+
+        <div className="d:w-1/3 pt-20 m-lg:pt-24">
+          <div className="callout h-full space-y-3 pt-0">
+            <div className="inline-block w-full">
+              <img
+                className="mx-auto h-40 w-40 -mt-20 m-lg:h-48 m-lg:w-48 m-lg:-mt-24 rounded-full object-cover"
+                src="https://www.goclimate.com/blog/wp-content/uploads/2017/05/0000213_trang-palm-oil-wastewater-treatment-project-in-trang-province-thailand_550.jpeg"
+              />
+            </div>
+            <h3 className="heading">
+              Trang Palm Oil Wastewater Treatment Project in Trang Province
+            </h3>
+            <p>
+              We have now made our third community climate offset, this time in
+              Trang Palm Oil Wastewater Treatment Project in Trang Province,
+              Thailand.
+            </p>
+          </div>
+        </div>
+      </div> */}
     </div>
+  </>
   );
 };
 
