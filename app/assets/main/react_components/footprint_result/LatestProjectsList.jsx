b0d1e5af7d147e10c0d63eb1c286b88c734c836b
Teodor Lennmark <tele@netlight.com>
2021-09-20 11:13:49 +0200

    Moved functionality to new component fpr projekt summary box

--- app/assets/main/react_components/footprint_result/LatestProjectsList.jsx
+++ app/assets/main/react_components/footprint_result/LatestProjectsList.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTexts } from '../context/Footprint/TextsContext';
 import { useProjects } from '../context/Footprint/ProjectsContext';
+import ProjectSummary from './ProjectSummary.jsx';
 
 const LatestProjectsList = () => {
   const {
@@ -8,49 +9,17 @@ const LatestProjectsList = () => {
   } = useTexts();
   const projects = useProjects();
 
-  const [collapseState, setCollapseState] = useState('hidden');
-
   return (
     <>
       <div className="space-y-3 text-left">
         <h3 className="font-semibold">{latest_projects}</h3>
         <div className="mt-12 flex flex-col d:flex-row">
           {projects.map((project) => (
-              <div key={project.id} className="d:w-1/3 pt-20 px-1 m-lg:pt-16">
-                <div className="callout h-full space-y-3 pt-0 p-4 text-left collapse">
-                  <div className="inline-block w-full">
-                    <img
-                      className="mx-auto h-10 w-10 -mt-10 m-lg:h-24 m-lg:w-24 m-lg:-mt-12 rounded-full object-cover"
-                      src={project.image_url}
-                    />
-                  </div>
-                  <h3 className="font-semibold">{project.name}</h3>
-                  <p>
-                    {collapseState === 'hidden'
-                      ? project.short_description.slice(0, 80) + '...'
-                      : project.short_description}
-                  </p>
-                  <div
-                    className="flex justify-between cursor-pointer"
-                    onClick={() =>
-                      setCollapseState(
-                        collapseState === 'hidden' ? 'block' : 'hidden'
-                      )
-                    }
-                  >
-                    <div className="font-semibold">Read more</div>
-                    <div className="pl-3">
-                      <i
-                        className={
-                          'fas fa-chevron-circle-down transition-transform ease-in-out duration-300 ' +
-                          (collapseState !== 'hidden' && 'transform rotate-180')
-                        }
-                        aria-hidden="true"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <ProjectSummary
+              key={project.id}
+              project={project}
+            />
+              
           ))}
         </div>
 
---
+++ app/assets/main/react_components/footprint_result/ProjectSummary.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const ProjectSummary = ({ key, project }) => {
+  const [collapseState, setCollapseState] = useState('hidden');
+
+  return (
+    <div key={key} className="d:w-1/3 pt-20 px-1 m-lg:pt-16">
+      <div className="callout h-full space-y-3 pt-0 p-4 text-left collapse">
+        <div className="inline-block w-full">
+          <img
+            className="mx-auto h-10 w-10 -mt-10 m-lg:h-24 m-lg:w-24 m-lg:-mt-12 rounded-full object-cover"
+            src={project.image_url}
+          />
+        </div>
+        <h3 className="font-semibold">{project.name}</h3>
+        <p>
+          {collapseState === 'hidden'
+            ? project.short_description.slice(0, 80) + '...'
+            : project.short_description}
+        </p>
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={() =>
+            setCollapseState(collapseState === 'hidden' ? 'block' : 'hidden')
+          }
+        >
+          <div className="font-semibold">Read more</div>
+          <div className="pl-3">
+            <i
+              className={
+                'fas fa-chevron-circle-down transition-transform ease-in-out duration-300 ' +
+                (collapseState !== 'hidden' && 'transform rotate-180')
+              }
+              aria-hidden="true"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
