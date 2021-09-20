9a19ec0d1476991db78c14398ec9def0d7c24789
Teodor Lennmark <tele@netlight.com>
2021-09-20 10:33:41 +0200

    Added read more clicker

--- app/assets/main/react_components/footprint_result/LatestProjectsList.jsx
+++ app/assets/main/react_components/footprint_result/LatestProjectsList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTexts } from '../context/Footprint/TextsContext';
 import { useProjects } from '../context/Footprint/ProjectsContext';
 
@@ -8,35 +8,55 @@ const LatestProjectsList = () => {
   } = useTexts();
   const projects = useProjects();
 
+  const [collapseState, setCollapseState] = useState('hidden');
+
   return (
     <>
-    <div className="space-y-3 text-left">
-      <h3 className="font-semibold">{latest_projects}</h3>
-      <div className="mt-12 flex flex-col d:flex-row">
-
-        {projects.map((project) => (
-          <>
-            <div className="d:w-1/3 pt-20 px-1 m-lg:pt-16">
-              <div className="callout h-full space-y-3 pt-0 p-4">
-                <div className="inline-block w-full">
-                  <img
-                    className="mx-auto h-10 w-10 -mt-10 m-lg:h-24 m-lg:w-24 m-lg:-mt-12 rounded-full object-cover"
-                    src={project.image_url}
-                  />
+      <div className="space-y-3 text-left">
+        <h3 className="font-semibold">{latest_projects}</h3>
+        <div className="mt-12 flex flex-col d:flex-row">
+          {projects.map((project) => (
+            <>
+              <div className="d:w-1/3 pt-20 px-1 m-lg:pt-16">
+                <div className="callout h-full space-y-3 pt-0 p-4 text-left collapse">
+                  <div className="inline-block w-full">
+                    <img
+                      className="mx-auto h-10 w-10 -mt-10 m-lg:h-24 m-lg:w-24 m-lg:-mt-12 rounded-full object-cover"
+                      src={project.image_url}
+                    />
+                  </div>
+                  <h3 className="font-semibold">{project.name}</h3>
+                  <p>
+                    {collapseState === 'hidden'
+                      ? project.short_description.slice(0, 80) + '...'
+                      : project.short_description}
+                  </p>
+                  <div
+                    className="flex justify-between cursor-pointer"
+                    onClick={() =>
+                      setCollapseState(
+                        collapseState === 'hidden' ? 'block' : 'hidden'
+                      )
+                    }
+                  >
+                    <div className="font-semibold">Read more</div>
+                    <div className="pl-3">
+                      <i
+                        className={
+                          'fas fa-chevron-circle-down transition-transform ease-in-out duration-300 ' +
+                          (collapseState !== 'hidden' && 'transform rotate-180')
+                        }
+                        aria-hidden="true"
+                      />
+                    </div>
+                  </div>
                 </div>
-                <h3 className="font-semibold">
-                  {project.name}
-                </h3>
-                <p>
-                  {project.short_description}
-                </p>
               </div>
-            </div>
-          </>
-        ))}
-      </div>
+            </>
+          ))}
+        </div>
 
-      {/* <li
+        {/* <li
             key={project.name}
             className="flex items-center p-3 bg-white rounded border border-gray-tint-2"
           >
@@ -51,7 +71,7 @@ const LatestProjectsList = () => {
               <p>{project.country}</p>
             </span>
           </li> */}
-      {/* <div className="mt-12 flex flex-col d:flex-row section-gutter">
+        {/* <div className="mt-12 flex flex-col d:flex-row section-gutter">
 
         <div className="d:w-1/3 pt-20 m-lg:pt-24">
           <div className="callout h-full space-y-3 pt-0">
@@ -91,8 +111,8 @@ const LatestProjectsList = () => {
           </div>
         </div>
       </div> */}
-    </div>
-  </>
+      </div>
+    </>
   );
 };
 
