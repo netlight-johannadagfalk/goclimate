3b4f040f6eb4e031136c0035cbd783e3dfb10ffe
Elina Olsson <elina.olsson@netlight.com>
2021-09-21 14:21:42 +0200

    Moved functionality of actions and achievements into new components

--- app/assets/main/react_components/dashboard/climate_actions/manage_actions/kanban/KanbanActionItem.jsx
+++ app/assets/main/react_components/dashboard/climate_actions/manage_actions/kanban/KanbanActionItem.jsx
@@ -14,7 +14,7 @@ const KanbanActionItem = ({
   const isBadge = item.userActionsArray ? true : false;
   const categoryName = () => {
     for (let i = 0; i <= Object.keys(categories).length; i++) {
-      if (isBadge) return "";
+      if (isAchievement) return "";
 
       if (categories[i].id === item.climate_action_category_id) {
         return categories[i].name.toString();
@@ -98,7 +98,7 @@ const KanbanActionItem = ({
                   item.expanded && "w-full border-b border-b-gray-tint-2"
                 }`}
               >
-                {!isBadge && (
+                {!isAchievement && (
                   <div
                     className={`${
                       "category_" +
@@ -114,7 +114,7 @@ const KanbanActionItem = ({
                   <div className="flex flex-1">
                     <div
                       className={`mx-auto ${
-                        isBadge ? "mt-2" : "-mt-1/4"
+                        isAchievement ? "mt-2" : "-mt-1/4"
                       } rounded-full h-16 w-16 items-center justify-center bg-contain bg-center shadow-lg`}
                       style={{
                         backgroundImage:
@@ -128,7 +128,7 @@ const KanbanActionItem = ({
                   <div className="flex flex-2 justify-start">
                     <div
                       className={`flex flex-1 font-bold text-left ${
-                        isBadge && "mt-5"
+                        isAchievement && "mt-5"
                       }
                       text-sm
                       `}
@@ -139,13 +139,13 @@ const KanbanActionItem = ({
                   <div className="flex flex-1 justify-center items-start">
                     <button
                       className={`fas float-right focus:outline-none ${
-                        isBadge ? "mt-12 ml-10" : "mt-4 ml-4"
+                        isAchievement ? "mt-12 ml-10" : "mt-4 ml-4"
                       } ${item.expanded ? "fa-chevron-up" : "fa-chevron-down"}`}
                       onClick={() => handleExpanded(item, !item.expanded)}
                     ></button>
                   </div>
                 </div>
-                {isBadge && (
+                {isAchievement && (
                   <div className="flex justify-center ml-6 -mt-4">
                     <ProgressBar
                       categories={categories}
---
+++ app/assets/main/react_components/dashboard/climate_actions/manage_actions/kanban/card_components/ActionCardExpanded.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActionCardExpanded = () => {
+  return <div></div>;
+};
+
+export default ActionCardExpanded;
