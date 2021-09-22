ea2d5c5116629afb79d98e606611dfba2c3d0e6d
Elina Olsson <elina.olsson@netlight.com>
2021-09-22 13:48:22 +0200

    Removed duplicate code in AchievementCard and changed name from item to achievement and action

--- app/assets/main/react_components/dashboard/climate_actions/manage_actions/kanban/card_components/AchievementCard.jsx
+++ app/assets/main/react_components/dashboard/climate_actions/manage_actions/kanban/card_components/AchievementCard.jsx
@@ -2,86 +2,58 @@ import React from "react";
 import ProgressBar from "../ProgressBar.jsx";
 
 const AchievementCard = ({
-  item,
+  achievement,
   categories,
   sidebarCollapsed,
   handleUncompleteAction,
 }) => {
+  const actions = [
+    ...achievement.userActionsArray,
+    ...achievement.actionsArray,
+  ];
+
   return (
     <div>
       <ProgressBar
         categories={categories}
-        item={item}
-        userActions={item.userActionsArray}
-        actions={item.actionsArray}
+        item={achievement}
+        userActions={achievement.userActionsArray}
+        actions={achievement.actionsArray}
       />
 
-      {item.expanded && !sidebarCollapsed && (
-        <div className="mb-4 mt-7 ml-7 mr-4">
-          <div>
-            {/* Map through both arrays in categoryBadge. All actions in the category should be rendered in a list, but those userActions that are performed should be highlighted */}
-            {item.userActionsArray.map((subitem) => {
-              return (
-                <div key={subitem.id}>
-                  {subitem.status === true ? (
-                    <div className="group flex items-center mt-1 mb-3">
-                      <div
-                        className="mr-3 rounded-full h-6 w-6 bg-cover flex-initial"
-                        style={{
-                          backgroundImage:
-                            "url('/achievement_images/AchievementStarActive.png')",
-                        }}
-                      ></div>
-                      <div
-                        className={`flex-initial text-left text-sm
-                                  `}
-                      >
-                        {subitem.name}
-                      </div>
-                      <button
-                        className="d:opacity-0 d:group-hover:opacity-50 d:hover:!opacity-100 opacity-50 flex-1 text-lg text-right"
-                        onClick={() => handleUncompleteAction(subitem)}
-                      >
-                        &times;
-                      </button>
-                    </div>
-                  ) : (
-                    <div className="flex mt-1 mb-3">
-                      <div
-                        className="mr-3 rounded-full h-6 w-6 bg-cover flex-initial"
-                        style={{
-                          backgroundImage:
-                            "url('/achievement_images/AchievementStarInactive.png')",
-                        }}
-                      ></div>
-                      <div
-                        className={`text-sm flex-inital text-left text-gray-accent
-                                  `}
-                      >
-                        {subitem.name}
-                      </div>
-                    </div>
-                  )}
-                </div>
-              );
-            })}
-          </div>
-
-          {item.actionsArray.map((subitem) => {
+      {achievement.expanded && !sidebarCollapsed && (
+        <div className="mb-4 mt-9 ml-7 mr-4">
+          {/* Map through both arrays in categoryBadge. All actions in the category should be rendered in a list, but those userActions that are performed should be highlighted */}
+          {actions.map((action) => {
             return (
-              <div key={subitem.id}>
-                <div className="flex mt-1 mb-3">
-                  <div
-                    className="mr-3 rounded-full h-6 w-6 bg-cover flex-initial"
-                    style={{
-                      backgroundImage:
-                        "url('/achievement_images/AchievementStarInactive.png')",
-                    }}
-                  ></div>
-                  <div className="text-gray-accent text-sm flex-inital text-left">
-                    {subitem.name}
-                  </div>
+              <div
+                className="group flex items-center mt-1 mb-3"
+                key={action.id}
+              >
+                <div
+                  className="mr-3 rounded-full h-6 w-6 bg-cover flex-initial"
+                  style={{
+                    backgroundImage:
+                      action.status === true
+                        ? "url('/achievement_images/AchievementStarActive.png')"
+                        : "url('/achievement_images/AchievementStarInactive.png')",
+                  }}
+                ></div>
+                <div
+                  className={`flex-initial text-left text-sm ${
+                    action.status ? "text-black" : "text-gray-accent"
+                  }`}
+                >
+                  {action.name}
                 </div>
+                {action.status && (
+                  <button
+                    className="d:opacity-0 d:group-hover:opacity-50 d:hover:!opacity-100 opacity-50 flex-1 text-lg text-right"
+                    onClick={() => handleUncompleteAction(action)}
+                  >
+                    &times;
+                  </button>
+                )}
               </div>
             );
           })}
