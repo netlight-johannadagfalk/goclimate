f560a12d4a0db4f34b798f1bb321701b6e198592
Teodor Lennmark <tele@netlight.com>
2021-09-22 14:43:06 +0200

    Add background blob for where money go

--- app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/common/MoneyUsageList.jsx
+++ app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/common/MoneyUsageList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useTexts } from '../../../contexts/TextsContext.js';
+// import blob from '../../../../../../../../bundles/images/shapes/blob_3.svg';
+import blob2 from './blob_5.svg';
 
 const MoneyUsageList = () => {
   const {
@@ -14,13 +16,18 @@ const MoneyUsageList = () => {
   );
 
   return (
-    <div className="space-y-3">
-      <h3 className="font-semibold">{heading}</h3>
-      <ul className="list-check space-y-3 text-left">
-        {listValues.map(([key, text]) => (
-          <li key={key}>{text}</li>
-        ))}
-      </ul>
+    <div className="mt-20 mb-16">
+      <div className="relative mx-auto w-full  ">
+        <img className="invisible t:visible w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" alt="" src={blob2}/>
+        <div className="relative space-y-3">
+          <h3 className="text-center heading">{heading}</h3>
+          <ul className="list-check space-y-3 text-left">
+              {listValues.map(([key, text]) => (
+              <li key={key}>{text}</li>
+            ))}  
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
