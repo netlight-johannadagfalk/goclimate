8f6e6c9083e6bc6733abd6a5a0d38ef067f19420
Teodor Lennmark <tele@netlight.com>
2021-09-22 16:01:08 +0200

    Fix path to image

--- app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/common/MoneyUsageList.jsx
+++ app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/common/MoneyUsageList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTexts } from '../../../contexts/TextsContext.js';
-// import blob from '../../../../../../../../bundles/images/shapes/blob_3.svg';
-import blob2 from './blob_5.svg';
+import blobImage from '../../../../../../../../assets/images/shapes/blob_5.svg';
 
 const MoneyUsageList = () => {
   const {
@@ -17,8 +16,8 @@ const MoneyUsageList = () => {
 
   return (
     <div className="mt-20 mb-16">
-      <div className="relative mx-auto w-full  ">
-        <img className="invisible t:visible w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" alt="" src={blob2}/>
+      <div className="relative mx-auto w-full">
+        <img className="invisible t:visible w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" alt="" src={blobImage}/>
         <div className="relative space-y-3">
           <h3 className="text-center heading">{heading}</h3>
           <ul className="list-check space-y-3 text-left">
