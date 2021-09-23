7932845bb9b21bad5eeb78a623a9a5a012d65f81
Sara Andersson <sara.andersson@netlight.com>
2021-09-23 14:18:28 +0200

    started working on refactoring css

---
+++ app/assets/main/react_components/dashboard/climate_actions/carousel/carousel_items/CategoryColor.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+const CategoryColor = ({ categories, action }) => {
+  const categoryName = () => {
+    for (let i = 0; i <= Object.keys(categories).length; i++) {
+      if (categories[i].id === action.climate_action_category_id) {
+        return categories[i].name.toString();
+      }
+    }
+    return "unknown";
+  };
+  return (
+    <div
+      className={`${
+        "category_" +
+        categoryName().toLowerCase().replace(/ /g, "_") +
+        "_active"
+      } h-7 w-full rounded-t border-t-gray-tint-2 bg-opacity-60`}
+    ></div>
+  );
+};
+export default CategoryColor;
