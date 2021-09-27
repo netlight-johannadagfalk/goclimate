ae52dc40cf62b85c216b1599886e6b66f36490fe
Teodor Lennmark <tele@netlight.com>
2021-09-27 11:34:18 +0200

    Recaforing if statement

--- app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/common/latest_projects_list/components/ProjectSummary.jsx
+++ app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/common/latest_projects_list/components/ProjectSummary.jsx
@@ -5,38 +5,48 @@ const ProjectSummary = ({ project }) => {
   const [collapseState, setCollapseState] = useState('hidden');
   const [shortText, setShortText] = useState(false);
   const [tempHeight, setTempHeight] = useState(0.0);
-  
+
   const {
     sharedText: { read_more },
   } = useTexts();
 
   useEffect(() => {
-    const titleElement = document.getElementById("title"+project.id)
-    const descriptionElement = document.getElementById("description"+project.id)
-    const projectBoxElement = document.getElementById("projectBox"+project.id)
-    setTempHeight(projectBoxElement?.clientHeight+3)
-    if (titleElement?.scrollHeight + descriptionElement?.scrollHeight < 125) {
-      setShortText(true)
-    }
+    const titleElement = document.getElementById('title' + project.id);
+    const descriptionElement = document.getElementById(
+      'description' + project.id
+    );
+    const projectBoxElement = document.getElementById(
+      'projectBox' + project.id
+    );
+    setTempHeight(projectBoxElement?.clientHeight + 3);
+    setShortText((titleElement?.scrollHeight + descriptionElement?.scrollHeight < 125));
   }, []);
 
   return (
-    <div className="t:w-1/3 pt-14 t:px-1 self-center t:self-start max-w-sm w-full" >
-      <div id={"projectBox"+project.id} className="callout space-y-3 pt-0 p-4 text-left collapse min-h-1/4 w-full" style={{minHeight: +tempHeight+"px", minWidth: "100%"}}>
+    <div className="t:w-1/3 pt-14 t:px-1 self-center t:self-start max-w-sm w-full">
+      <div
+        id={'projectBox' + project.id}
+        className="callout space-y-3 pt-0 p-4 text-left collapse min-h-1/4 w-full"
+        style={{ minHeight: +tempHeight + 'px' }}
+      >
         <div className="inline-block w-full ">
           <img
             className="mx-auto h-20 w-20 -mt-10 m-lg:h-24 m-lg:w-24 m-lg:-mt-12 rounded-full object-cover"
             src={project.image_url}
           />
         </div>
-        
+
         <div
           className={collapseState === 'hidden' ? ' fadeProjects w-full' : ''}
         >
-          <h3 className="font-semibold" id={"title"+project.id}>{project.name}</h3>
-          <p className="pt-2" id={"description"+project.id}>{project.short_description}</p>
+          <h3 className="font-semibold" id={'title' + project.id}>
+            {project.name}
+          </h3>
+          <p className="pt-2" id={'description' + project.id}>
+            {project.short_description}
+          </p>
         </div>
-        { !shortText &&
+        {!shortText && (
           <div
             className="flex cursor-pointer justify-center"
             onClick={() =>
@@ -54,7 +64,7 @@ const ProjectSummary = ({ project }) => {
               />
             </div>
           </div>
-        }
+        )}
       </div>
     </div>
   );
