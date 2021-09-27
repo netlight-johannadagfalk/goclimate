e852e9532cef7b19cb575c21b8c6123290ececdc
Teodor Lennmark <tele@netlight.com>
2021-09-27 11:30:11 +0200

    Fixing with in mobile view

--- app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/common/latest_projects_list/components/ProjectSummary.jsx
+++ app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/common/latest_projects_list/components/ProjectSummary.jsx
@@ -5,49 +5,24 @@ const ProjectSummary = ({ project }) => {
   const [collapseState, setCollapseState] = useState('hidden');
   const [shortText, setShortText] = useState(false);
   const [tempHeight, setTempHeight] = useState(0.0);
-  const [tempHeight2, setTempHeight2] = useState(0.0);
-
   
   const {
     sharedText: { read_more },
   } = useTexts();
 
-  const HEJ = (
-    <>
-      <h3 className="font-semibold" id={"test"+project.id}>      {tempHeight2}
-{project.name}</h3>
-      <p className="pt-2" id={"testa"+project.id}>{project.short_description}</p>
-    </>
-  )
-
   useEffect(() => {
-    const element = document.getElementById("test"+project.id)
-    console.log(element?.scrollHeight)
-
-    const element2 = document.getElementById("testa"+project.id)
-    console.log(element2?.scrollHeight)
-
-    const element3 = document.getElementById("testq"+project.id)
-    console.log("q3 ",element3?.scrollHeight)
-    setTempHeight(element3?.scrollHeight)
-
-    const element4 = document.getElementById("testr"+project.id)
-    console.log("q4 ",element4?.scrollHeight)
-    setTempHeight2(element4?.clientHeight)
-
-    console.log("tempHeight", tempHeight);
-    console.log("tempHeight2", tempHeight2);
-
-    if (element?.scrollHeight + element2?.scrollHeight < 125) {
-      console.log("haj ",element?.scrollHeight + element2?.scrollHeight);
+    const titleElement = document.getElementById("title"+project.id)
+    const descriptionElement = document.getElementById("description"+project.id)
+    const projectBoxElement = document.getElementById("projectBox"+project.id)
+    setTempHeight(projectBoxElement?.clientHeight+3)
+    if (titleElement?.scrollHeight + descriptionElement?.scrollHeight < 125) {
       setShortText(true)
     }
-    
   }, []);
 
   return (
-    <div id={"testq"+project.id} className="t:w-1/3 pt-14 t:px-1 self-center t:self-start" >
-      <div id={"testr"+project.id} className="callout space-y-3 pt-0 p-4 text-left collapse min-h-1/4 max-w-sm" style={{minHeight: +tempHeight2+3+"px"}}>
+    <div className="t:w-1/3 pt-14 t:px-1 self-center t:self-start max-w-sm w-full" >
+      <div id={"projectBox"+project.id} className="callout space-y-3 pt-0 p-4 text-left collapse min-h-1/4 w-full" style={{minHeight: +tempHeight+"px", minWidth: "100%"}}>
         <div className="inline-block w-full ">
           <img
             className="mx-auto h-20 w-20 -mt-10 m-lg:h-24 m-lg:w-24 m-lg:-mt-12 rounded-full object-cover"
@@ -58,9 +33,8 @@ const ProjectSummary = ({ project }) => {
         <div
           className={collapseState === 'hidden' ? ' fadeProjects w-full' : ''}
         >
-          {
-            HEJ
-          }
+          <h3 className="font-semibold" id={"title"+project.id}>{project.name}</h3>
+          <p className="pt-2" id={"description"+project.id}>{project.short_description}</p>
         </div>
         { !shortText &&
           <div
