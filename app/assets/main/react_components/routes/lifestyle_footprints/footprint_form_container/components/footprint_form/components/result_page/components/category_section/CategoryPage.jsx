fa02bfc1d89d497d473d60728d301f00350c6653
Teodor Lennmark <tele@netlight.com>
2021-09-28 09:17:34 +0200

    Inserted for world_page and category page

--- app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/footprint_form/components/result_page/components/category_section/CategoryPage.jsx
+++ app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/footprint_form/components/result_page/components/category_section/CategoryPage.jsx
@@ -7,8 +7,10 @@ import Title from '../../../common/Title.jsx';
 const CategoryPage = ({ footprint }) => {
   const {
     reactContentText: {
-      category_chart: { title, desc },
-      public_emissions,
+      category_page: {
+        category_chart: { title, desc },
+        public_emissions,
+      },
     },
   } = useTexts();
 
