14bc055f8554aaa44bcb1e22045ee3b365efa302
e-ry <erya@netlight.com>
2021-09-28 14:37:23 +0200

    preamble public emissions belongs to v2

--- app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/footprint_form/components/result_page/components/category_section/CategoryPage.jsx
+++ app/assets/main/react_components/routes/lifestyle_footprints/footprint_form_container/components/footprint_form/components/result_page/components/category_section/CategoryPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTexts } from '../../../../../../../contexts/TextsContext.js';
+import { useVersion } from '../../../../../../../contexts/VersionContext.js';
 import Preamble from '../../../common/Preamble.jsx';
 import Title from '../../../common/Title.jsx';
 import CategoryChart from './components/CategoryChart.jsx';
@@ -12,15 +13,21 @@ const CategoryPage = ({ footprint }) => {
     },
   } = useTexts();
 
+  const version = useVersion();
+
   return (
     <div className="max-w-lg mx-auto">
       <Title custom_style="text-lgr" text={title} />
       <Preamble text={desc} />
       <CategoryChart footprint={footprint} />
-      <br></br>
-      <div className="text-left">
-        <Preamble text={public_emissions} />
-      </div>
+      {version === 'v1' && (
+        <>
+          <br></br>
+          <div className="text-left">
+            <Preamble text={public_emissions} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
