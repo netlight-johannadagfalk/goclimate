362b866dae34bc4dc657035c865d821e8feb3bfd
nanditagoclimate <89521136+nanditagoclimate@users.noreply.github.com>
2021-09-29 17:44:08 +0530

    Migration to remove input_type from unit table (#575)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_062003) do
+ActiveRecord::Schema.define(version: 2021_09_28_092550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2021_09_27_062003) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "key"
     t.text "name"
-    t.text "input_type"
   end
 
   create_table "card_charges", force: :cascade do |t|
